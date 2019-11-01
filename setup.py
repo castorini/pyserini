7036225e7fd08306b95673b0a9d7141898f38259
lintool <jimmylin@uwaterloo.ca>
2019-11-01 19:39:22 -0400

    Build script.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyserini",
+    version="0.6.0_1",
+    author="Jimmy Lin",
+    author_email="jimmylin@uwaterloo.ca",
+    description="Python interface to Anserini",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/castorini/pyserini",
+    install_requires=['Cython', 'pyjnius'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
