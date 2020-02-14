0ddd5db5b27605474baae4f37235fda7b9af1e3f
Jimmy Lin <jimmylin@uwaterloo.ca>
2020-02-14 16:21:50 -0500

    Minor refactoring to conform to PEP 8 (#36)

--- pyserini/pyclass.py
+++ pyserini/pyclass.py
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-'''
+"""
 Module for hiding Python-Java calls via Pyjnius
-'''
+"""
 
 ### Pyjnius setup
 
