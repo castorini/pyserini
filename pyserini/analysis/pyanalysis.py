b273a03b9f993dc53bab8d4e071262402a55d703
chriskamphuis <mail@chriskamphuis.com>
2020-03-06 16:15:50 +0100

    Restructure file according to PR feedback

--- pyserini/analysis/pyanalysis.py
+++ pyserini/analysis/pyanalysis.py
@@ -11,121 +11,42 @@ from ..pyclass import JTokenizeOnlyAnalyzer
 from ..pyclass import JTweetAnalyzer
 
 
-def get_default_analyzer():
+def get_analyzer(analyzer, stemmer='porter'):
     """
+    Parameters
+    ----------
+    analyzer : String
+        Name of analyzer to get
+
+    stemmer : String
+        Name of stemmer that analyzer needs to use (not all analyzers allow for a stemmer to be provided)
 
     Returns
     -------
-    JEnglishStemmingAnalyzer
+    result : org.apache.lucene.document.Analyzer
+        Java Analyzer object
     """
-    return get_english_stemming_analyzer()
-
-
-def get_arabic_analyzer():
-    """
-
-    Returns
-    -------
-    JArabicAnalyzer
-    """
-    return JArabicAnalyzer()
-
-
-def get_bengali_analyzer():
-    """
-
-    Returns
-    -------
-    JBengaliAnalyzer
-    """
-    return JBengaliAnalyzer()
-
-
-def get_cjk_analyzer():
-    """
-
-    Returns
-    -------
-    JCJKAnalyzer
-    """
-    return JCJKAnalyzer()
-
-
-def get_german_analyzer():
-    """
-
-    Returns
-    -------
-    JGermanAnalyzer
-    """
-    return JGermanAnalyzer()
-
-
-def get_spanish_analyzer():
-    """
-
-    Returns
-    -------
-    JSpanishAnalyzer
-    """
-    return JSpanishAnalyzer()
-
-
-def get_french_analyzer():
-    """
-
-    Returns
-    -------
-    JFrenchAnalyzer
-    """
-    return JFrenchAnalyzer()
-
-
-def get_hindi_analyzer():
-    """
-
-    Returns
-    -------
-    JHindiAnalyzer
-    """
-    return JHindiAnalyzer()
-
-
-def get_english_stemming_analyzer(stemmer='porter'):
-    """
-
-    Returns
-    -------
-    JEnglishStemmingAnalyzer
-    """
-    return JEnglishStemmingAnalyzer(stemmer)
-
-
-def get_freebase_analyzer():
-    """
-
-    Returns
-    -------
-    JFreebaseAnalyzer
-    """
-    return JFreebaseAnalyzer()
-
-
-def get_tokenize_only_analyzer():
-    """
-
-    Returns
-    -------
-    JTokenizeOnlyAnalyzer
-    """
-    return JTokenizeOnlyAnalyzer()
-
-
-def get_tweet_analyzer():
-    """
-
-    Returns
-    -------
-    JTweetAnalyzer
-    """
-    return JTweetAnalyzer()
+    if analyzer == 'arabic':
+        return JArabicAnalyzer()
+    elif analyzer == 'bengali':
+        return JBengaliAnalyzer()
+    elif analyzer == 'cjk':
+        return JCJKAnalyzer()
+    elif analyzer == 'german':
+        return JGermanAnalyzer()
+    elif analyzer == 'spanish':
+        return JSpanishAnalyzer()
+    elif analyzer == 'french':
+        return JFrenchAnalyzer()
+    elif analyzer == 'hindi':
+        return JHindiAnalyzer()
+    elif analyzer == 'english':
+        return JEnglishStemmingAnalyzer(stemmer)
+    elif analyzer == 'freebase':
+        return JFreebaseAnalyzer()
+    elif analyzer == 'tokenize':
+        return JTokenizeOnlyAnalyzer()
+    elif analyzer == 'tweet':
+        return JTweetAnalyzer()
+    else:
+        return JEnglishStemmingAnalyzer('porter')
