6519bc9a4be42e7d0ba98678cd4efaa52e2eca9c
yuki617 <yuqiliu899@gmail.com>
2020-05-18 20:48:23 -0400

    add simplesearcher

--- pyserini/search/pysearch.py
+++ pyserini/search/pysearch.py
@@ -376,32 +376,6 @@ class SimpleNearestNeighborSearcher:
 
 if __name__ == "__main__":
 
-    def main(index, topics, output):
-        searcher = SimpleSearcher(index)
-        my_list = []
-        if topics == 'robust04':
-            topics_dic = get_topics('robust04')
-            for key, value in sorted(topics_dic.items()):
-                search = value.get('title')
-                hits = searcher.search(search, 1000)
-                for i in range(0, len(hits)):
-                    my_list.append(f'{key} Q0 {hits[i].docid.strip()} {i + 1} {hits[i].score:.6f} Anserini')
-        else:
-            with open(topics, 'r') as content_file:
-                content = content_file.read()
-            topics_lst = re.findall(r'(?<=<top>)(.+?)(?=<desc>)', content, flags=re.S)
-            topics_lst = [i.strip() for i in topics_lst]
-            for topic in topics_lst:
-                number_search = topic.split("<title>")
-                number = number_search[0].strip().split(" ")[-1]
-                search = number_search[1].strip()
-                hits = searcher.search(search, 1000)
-                for i in range(0, len(hits)):
-                    my_list.append(f'{number} Q0 {hits[i].docid.strip()} {i + 1} {hits[i].score:.6f} Anserini')
-        with open(output, 'w') as f:
-            for item in my_list:
-                f.write("%s\n" % item)
-
     parser = argparse.ArgumentParser(description='Create a input schema')
     parser.add_argument('-index', metavar='path', required=True,
                         help='the path to workspace')
@@ -410,4 +384,28 @@ if __name__ == "__main__":
     parser.add_argument('-output', metavar='path', required=True,
                         help='path to the output file')
     args = parser.parse_args()
-    main(index=args.index, topics=args.topics, output=args.output)
+
+    searcher = SimpleSearcher(args.index)
+    topics_dic = get_topics(args.topics)
+    if topics_dic != {}:
+        target_file = open(args.output, 'w')
+        for key, value in sorted(topics_dic.items()):
+            search = value.get('title')
+            hits = searcher.search(search, 1000)
+            for i in range(0, len(hits)):
+                target_file.write(f'{key} Q0 {hits[i].docid.strip()} {i + 1} {hits[i].score:.6f} Anserini\n')
+        target_file.close()
+    else:
+        with open(args.topics, 'r') as content_file:
+            content = content_file.read()
+        topics_lst = re.findall(r'(?<=<top>)(.+?)(?=<desc>)', content, flags=re.S)
+        topics_lst = [i.strip() for i in topics_lst]
+        target_file = open(args.output, 'w')
+        for topic in topics_lst:
+            number_search = topic.split("<title>")
+            number = number_search[0].strip().split(" ")[-1]
+            search = number_search[1].strip()
+            hits = searcher.search(search, 1000)
+            for i in range(0, len(hits)):
+                target_file.write(f'{number} Q0 {hits[i].docid.strip()} {i + 1} {hits[i].score:.6f} Anserini\n')
+        target_file.close()
