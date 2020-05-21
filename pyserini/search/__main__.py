35cfdd2aa2ffdac89468b9bbeea37be6460e5811
x65han <x65han@uwaterloo.ca>
2020-05-21 15:31:24 -0700

    reranker version 1

--- pyserini/search/__main__.py
+++ pyserini/search/__main__.py
@@ -3,54 +3,7 @@ import argparse
 import enum
 
 from pyserini.search.pysearch import get_topics, SimpleSearcher
-from ..vectorizer import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-
-
-def get_prf_vectors(doc_ids, vectorizer, r=10, n=100):
-    train_docs = doc_ids[:r] + doc_ids[-n:]
-    train_labels = [1] * r + [0] * n
-
-    train_vectors = vectorizer.get_vectors(train_docs)
-    test_vectors = vectorizer.get_vectors(doc_ids)
-
-    return train_vectors, train_labels, test_vectors
-
-
-# sort both list in decreasing order by using the list1 to compare
-def sort_dual_list(list1, list2):
-    zipped_lists = zip(list1, list2)
-    sorted_pairs = sorted(zipped_lists)
-
-    tuples = zip(*sorted_pairs)
-    list1, list2 = [list(tuple) for tuple in tuples]
-
-    list1.reverse()
-    list2.reverse()
-    return list1, list2
-
-
-def normalize(scores):
-    low = min(scores)
-    high = max(scores)
-    width = high - low
-
-    return [(s-low)/width for s in scores]
-
-
-class ClassifierType(enum.Enum):
-    LR = 'lr'
-    SVM = 'svm'
-
-
-def get_clasifier(type: ClassifierType):
-    if type == ClassifierType.LR:
-        return LogisticRegression()
-    elif type == ClassifierType.SVM:
-        return SVC(kernel='linear', probability=True)
-    else:
-        return None
+from pyserini.search.reranker import ClassifierType, PseudoRelevanceClassifierReranker
 
 
 parser = argparse.ArgumentParser(description='Create a input schema')
@@ -64,7 +17,7 @@ parser.add_argument('-rm3',  action='store_true',
                     help='use rm3 ranker')
 parser.add_argument('-qld',  action='store_true',
                     help='use qld ranker')
-parser.add_argument('-prf',  type=ClassifierType,
+parser.add_argument('-prf',  type=ClassifierType, nargs='+',
                     help='use pseudo relevance feedback ranker')
 parser.add_argument('-r',  type=int, default=10,
                     help='number of positive labels in pseudo relevance feedback')
@@ -81,32 +34,18 @@ if topics_dic == {}:
     print('Topic Not Found')
     exit()
 
-num_topics = len(topics_dic)
-vectorizer = TfidfVectorizer(args.index, min_df=5) if args.prf else None
 with open(args.output, 'w') as target_file:
     for index, topic in enumerate(sorted(topics_dic.keys())):
-        print(f'Topic {topic}: {index + 1}/{num_topics}')
+        print(f'Topic {topic}: {index + 1}/{len(topics_dic)}')
         search = topics_dic[topic].get('title')
         hits = searcher.search(search, 1000)
         doc_ids = [hit.docid.strip() for hit in hits]
         scores = [hit.score for hit in hits]
 
-        if args.prf and args.alpha > 0 and len(hits) > (args.r + args.n):
-            # vectorize
-            train_vectors, train_labels, test_vectors = get_prf_vectors(
-                doc_ids, vectorizer, r=args.r, n=args.n)
-
-            # classification
-            clf = get_clasifier(args.prf)
-            clf.fit(train_vectors, train_labels)
-            pred = clf.predict_proba(test_vectors)
-            rank_scores = normalize([p[1] for p in pred])
-
-            # interpolation
-            search_scores = normalize([hit.score for hit in hits])
-            interpolated_scores = [a * args.alpha + b * (1-args.alpha)
-                                   for a, b in zip(rank_scores, search_scores)]
-            scores, doc_ids = sort_dual_list(interpolated_scores, doc_ids)
+        if args.prf and len(args.prf) > 0 and args.alpha > 0 and len(hits) > (args.r + args.n):
+            ranker = PseudoRelevanceClassifierReranker(
+                args.index, args.prf, r=args.r, n=args.n, alpha=args.alpha)
+            scores, doc_ids = ranker.rerank(doc_ids, scores)
 
         tag = f'{args.prf}-A{args.alpha}' if args.prf else 'Anserini'
         for i, (doc_id, score) in enumerate(zip(doc_ids, scores)):
---
+++ pyserini/search/reranker.py
@@ -0,0 +1,106 @@
+import enum
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from typing import List
+from ..vectorizer import TfidfVectorizer
+import uuid
+import os
+
+
+class ClassifierType(enum.Enum):
+    LR = 'lr'
+    SVM = 'svm'
+
+
+class FusionMethod(enum.Enum):
+    AVG = 'avg'
+
+
+class PseudoRelevanceClassifierReranker:
+    def __init__(self, lucene_index: str, clf_type: List[ClassifierType], r=10, n=100, alpha=1):
+        self.r = r
+        self.n = n
+        self.alpha = alpha
+        self.clf_type = clf_type
+
+        if len(clf_type) > 2:
+            raise Exception('Re-ranker takes at most two classifiers')
+
+        self.vectorizer = TfidfVectorizer(lucene_index, min_df=5)
+
+    def set_classifier(self, clf_type: ClassifierType):
+        if clf_type == ClassifierType.LR:
+            self.clf = LogisticRegression()
+        elif clf_type == ClassifierType.SVM:
+            self.clf = SVC(kernel='linear', probability=True)
+        else:
+            raise Exception("Invalid classifier type")
+
+    def get_prf_vectors(self, doc_ids: List[str]):
+        train_docs = doc_ids[:self.r] + doc_ids[-self.n:]
+        train_labels = [1] * self.r + [0] * self.n
+
+        train_vecs = self.vectorizer.get_vectors(train_docs)
+        test_vecs = self.vectorizer.get_vectors(doc_ids)
+
+        return train_vecs, train_labels, test_vecs
+
+    def rerank_with_classifier(self, doc_ids: List[str], search_scores: List[float]):
+        train_vecs, train_labels, test_vecs = self.get_prf_vectors(
+            doc_ids)
+
+        # classification
+        self.clf.fit(train_vecs, train_labels)
+        pred = self.clf.predict_proba(test_vecs)
+        rank_scores = self.normalize([p[1] for p in pred])
+
+        # interpolation
+        interpolated_scores = [a * self.alpha + b * (1-self.alpha)
+                               for a, b in zip(rank_scores, search_scores)]
+
+        return self.sort_dual_list(interpolated_scores, doc_ids)
+
+    def rerank(self, doc_ids: List[str], search_scores: List[float]):
+        # one classifier
+        if len(self.clf_type) == 1:
+            self.set_classifier(self.clf_type[0])
+            return self.rerank_with_classifier(doc_ids, search_scores)
+
+        # two classifier with FusionMethod.AVG
+        doc_score_dict = {}
+        for i in range(2):
+            self.set_classifier(self.clf_type[i])
+            i_scores, i_doc_ids = self.rerank_with_classifier(
+                doc_ids, search_scores)
+
+            for score, doc_id in zip(i_scores, i_doc_ids):
+                if doc_id not in doc_score_dict:
+                    doc_score_dict[doc_id] = set()
+                doc_score_dict[doc_id].add(score)
+
+        r_scores, r_doc_ids = [], []
+        for doc_id, score in doc_score_dict.items():
+            avg = sum(score) / len(score)
+            r_doc_ids.append(doc_id)
+            r_scores.append(avg)
+
+        return r_scores, r_doc_ids
+
+    def normalize(self, scores: List[float]):
+        low = min(scores)
+        high = max(scores)
+        width = high - low
+
+        return [(s-low)/width for s in scores]
+
+    # sort both list in decreasing order by using the list1 to compare
+    def sort_dual_list(self, list1, list2):
+        zipped_lists = zip(list1, list2)
+        sorted_pairs = sorted(zipped_lists)
+
+        tuples = zip(*sorted_pairs)
+        list1, list2 = [list(tuple) for tuple in tuples]
+
+        list1.reverse()
+        list2.reverse()
+        return list1, list2
