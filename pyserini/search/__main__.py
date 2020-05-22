88be80a947eefe670dcb771d87e8169e7e7c8a49
x65han <x65han@uwaterloo.ca>
2020-05-21 19:18:25 -0700

    Re-use re-ranker in between topics

--- pyserini/search/__main__.py
+++ pyserini/search/__main__.py
@@ -36,6 +36,11 @@ if topics_dic == {}:
     print('Topic Not Found')
     exit()
 
+need_classifier = args.prf and len(args.prf) > 0 and args.alpha > 0
+if need_classifier is True:
+    ranker = PseudoRelevanceClassifierReranker(
+        args.index, args.prf, r=args.r, n=args.n, alpha=args.alpha)
+
 with open(args.output, 'w') as target_file:
     for index, topic in enumerate(sorted(topics_dic.keys())):
         print(f'Topic {topic}: {index + 1}/{len(topics_dic)}')
@@ -44,7 +49,7 @@ with open(args.output, 'w') as target_file:
         doc_ids = [hit.docid.strip() for hit in hits]
         scores = [hit.score for hit in hits]
 
-        if args.prf and len(args.prf) > 0 and args.alpha > 0 and len(hits) > (args.r + args.n):
+        if need_classifier and len(hits) > (args.r + args.n):
             ranker = PseudoRelevanceClassifierReranker(
                 args.index, args.prf, r=args.r, n=args.n, alpha=args.alpha)
             scores, doc_ids = ranker.rerank(doc_ids, scores)
