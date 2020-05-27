847753c4a113a141c4baee2582e8887b7a88b6b9
Johnson Han <x65han@uwaterloo.ca>
2020-05-26 21:17:49 -0700

    pyserini/search/__main__.py should use tqdm for progress update and have better args naming (#140)
    * pyserini/search/__main__.py
    
    * tests/test_indexutils.py should use path join instead of str concat
    
    * Revert test_indexutils.py & better variable naming in __main__.py
    
    * Add new line at the end of file in test_indexutils.py

--- pyserini/search/__main__.py
+++ pyserini/search/__main__.py
@@ -16,35 +16,33 @@
 
 import argparse
 import re
+from tqdm import tqdm
 from pyserini.search.pysearch import get_topics, SimpleSearcher
 from pyserini.search.reranker import ClassifierType, PseudoRelevanceClassifierReranker
 
-
 parser = argparse.ArgumentParser(description='Create a input schema')
-parser.add_argument('-index', metavar='path', required=True,
-                    help='the path to workspace')
-parser.add_argument('-topics', metavar='topicsname', required=True,
-                    help='topicsname')
-parser.add_argument('-output', metavar='path',
+parser.add_argument('-index', type=str, metavar='path to index',
+                    required=True, help='the path to lucene index')
+parser.add_argument('-topics', type=str, metavar='topic_name', required=True,
+                    help='topics name e.g. robust04, robust05, core17 & core18')
+parser.add_argument('-output', type=str, metavar='path',
                     help='path to the output file')
 parser.add_argument('-bm25',  action='store_true', default=True,
-                    help='use bm25 ranker')
-parser.add_argument('-rm3',  action='store_true',
-                    help='use rm3 ranker')
-parser.add_argument('-qld',  action='store_true',
-                    help='use qld ranker')
-parser.add_argument('-prf',  type=ClassifierType, nargs='+',
-                    help='use pseudo relevance feedback ranker')
-parser.add_argument('-r',  type=int, default=10,
+                    help='use bm25 ranker by default')
+parser.add_argument('-rm3',  action='store_true', help='use rm3 ranker')
+parser.add_argument('-qld',  action='store_true', help='use qld ranker')
+parser.add_argument('-prcl',  type=ClassifierType, nargs='+', default=[],
+                    help='specify the classifier PseudoRelevanceClassifierReranker uses')
+parser.add_argument('-prcl.r',  dest='r', type=int, default=10,
                     help='number of positive labels in pseudo relevance feedback')
-parser.add_argument('-n',  type=int, default=100,
+parser.add_argument('-prcl.n', dest='n', type=int, default=100,
                     help='number of negative labels in pseudo relevance feedback')
-parser.add_argument('-alpha',  type=float, default=0.5,
+parser.add_argument('-prcl.alpha', dest='alpha', type=float, default=0.5,
                     help='alpha value for interpolation in pseudo relevance feedback')
 args = parser.parse_args()
 
 searcher = SimpleSearcher(args.index)
-topics_dic = get_topics(args.topics)
+topics = get_topics(args.topics)
 search_rankers = ['bm25']
 if args.rm3:
     search_rankers.append('rm3')
@@ -53,41 +51,46 @@ if args.qld:
     search_rankers.append('qld')
     searcher.set_qld()
 
-if topics_dic == {}:
-    print('Topic Not Found')
+# invalid topics name
+if topics == {}:
+    print(f'Topic {args.topics} Not Found')
     exit()
 
+# get re-ranker
+use_prcl = args.prcl and len(args.prcl) > 0 and args.alpha > 0
+if use_prcl is True:
+    ranker = PseudoRelevanceClassifierReranker(
+        args.index, args.prcl, r=args.r, n=args.n, alpha=args.alpha)
+
+# build output path
 output_path = args.output
 if output_path is None:
-    clf_rankers = []
-    for t in args.prf:
-        if t == ClassifierType.LR:
-            clf_rankers.append('lr')
-        elif t == ClassifierType.SVM:
-            clf_rankers.append('svm')
+    if use_prcl is True:
+        clf_rankers = []
+        for t in args.prcl:
+            if t == ClassifierType.LR:
+                clf_rankers.append('lr')
+            elif t == ClassifierType.SVM:
+                clf_rankers.append('svm')
 
-    tokens = [args.topics, '+'.join(clf_rankers),
-              f'A{args.alpha}', '+'.join(search_rankers)]
-    output_path = '_'.join(tokens) + ".txt"
-
-need_classifier = args.prf and len(args.prf) > 0 and args.alpha > 0
-if need_classifier is True:
-    ranker = PseudoRelevanceClassifierReranker(
-        args.index, args.prf, r=args.r, n=args.n, alpha=args.alpha)
+        tokens = [args.topics, '+'.join(clf_rankers), f'A{args.alpha}', '+'.join(search_rankers)]
+        output_path = '_'.join(tokens) + ".txt"
+    else:
+        tokens = [args.topics, '+'.join(search_rankers)]
+        output_path = '_'.join(tokens) + ".txt"
 
 print('Output ->', output_path)
+
 with open(output_path, 'w') as target_file:
-    for index, topic in enumerate(sorted(topics_dic.keys())):
-        print(f'Topic {topic}: {index + 1}/{len(topics_dic)}')
-        search = topics_dic[topic].get('title')
+    for index, topic in enumerate(tqdm(sorted(topics.keys()))):
+        search = topics[topic].get('title')
         hits = searcher.search(search, 1000)
         doc_ids = [hit.docid.strip() for hit in hits]
         scores = [hit.score for hit in hits]
 
-        if need_classifier and len(hits) > (args.r + args.n):
+        if use_prcl and len(hits) > (args.r + args.n):
             scores, doc_ids = ranker.rerank(doc_ids, scores)
 
         tag = output_path[:-4] if args.output is None else 'Anserini'
         for i, (doc_id, score) in enumerate(zip(doc_ids, scores)):
-            target_file.write(
-                f'{topic} Q0 {doc_id} {i + 1} {score:.6f} {tag}\n')
+            target_file.write(f'{topic} Q0 {doc_id} {i + 1} {score:.6f} {tag}\n')
