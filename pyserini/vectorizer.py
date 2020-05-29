0ec4f3281a32f02584509917804708e1cc835c4c
x65han <x65han@uwaterloo.ca>
2020-05-29 12:56:22 -0700

    Introduce new BM25Vectorizer

--- pyserini/vectorizer.py
+++ pyserini/vectorizer.py
@@ -22,7 +22,39 @@ from pyserini.search import pysearch
 from pyserini.index import pyutils
 
 
-class TfidfVectorizer:
+class VectorizerBase:
+    def __init__(self, lucene_index_path: str, min_df: int = 1, verbose: bool = False) -> None:
+        self.min_df: int = min_df
+        self.verbose: bool = verbose
+        self.index_utils = pyutils.IndexReaderUtils(lucene_index_path)
+        self.searcher = pysearch.SimpleSearcher(lucene_index_path)
+        self.num_docs: int = self.searcher.num_docs
+
+        # build vocabulary
+        self.vocabulary_ = set()
+        self.idf_ = {}
+        for term in self.index_utils.terms():
+            self.idf_[term.term] = math.log(self.num_docs / term.df)
+            if term.df > self.min_df:
+                self.vocabulary_.add(term.term)
+
+        # build term to index mapping
+        self.term_to_index = {}
+        for index, term in enumerate(self.vocabulary_):
+            self.term_to_index[term] = index
+        self.vocabulary_size = len(self.vocabulary_)
+
+        if self.verbose:
+            print(f'Found {self.vocabulary_size} terms')
+
+    def _l2normalize(self, a):
+        norm_rows = np.sqrt(np.add.reduceat(a.data * a.data, a.indptr[:-1]))
+        nnz_per_row = np.diff(a.indptr)
+        a.data /= np.repeat(norm_rows, nnz_per_row)
+        return a
+
+
+class TfidfVectorizer(VectorizerBase):
     """Wrapper class for tf-idf vectorizer implemented on top of Pyserini.
 
     Parameters
@@ -36,36 +68,7 @@ class TfidfVectorizer:
     """
 
     def __init__(self, lucene_index_path: str, min_df: int = 1, verbose: bool = False) -> None:
-        self.verbose: bool = verbose
-        self.min_df: int = min_df
-        self.index_utils = pyutils.IndexReaderUtils(lucene_index_path)
-
-        # get num_docs
-        self.searcher = pysearch.SimpleSearcher(lucene_index_path)
-        self.num_docs: int = self.searcher.num_docs
-
-        # pre-processing
-        self.vocabulary_ = set()
-        self.idf_ = {}
-
-        for term in self.index_utils.terms():
-            self.idf_[term.term] = math.log(self.num_docs / term.df)
-            if term.df > self.min_df:
-                self.vocabulary_.add(term.term)
-
-        self.term_to_index = {}
-        for index, term in enumerate(self.vocabulary_):
-            self.term_to_index[term] = index
-        self.vocabulary_size = len(self.vocabulary_)
-
-        if self.verbose:
-            print(f'Found {self.vocabulary_size} terms')
-
-    def l2norm(self, a):
-        norm_rows = np.sqrt(np.add.reduceat(a.data * a.data, a.indptr[:-1]))
-        nnz_per_row = np.diff(a.indptr)
-        a.data /= np.repeat(norm_rows, nnz_per_row)
-        return a
+        super().__init__(lucene_index_path, verbose, min_df)
 
     def get_vectors(self, doc_ids: List[str]):
         """Get the tf-idf vectors given a list of doc_ids
@@ -103,4 +106,53 @@ class TfidfVectorizer:
                 matrix_data.append(tfidf)
 
         vectors = csr_matrix((matrix_data, (matrix_row, matrix_col)), shape=(num_docs, self.vocabulary_size))
-        return self.l2norm(vectors)
+        return self._l2normalize(vectors)
+
+
+class BM25Vectorizer(VectorizerBase):
+    """Wrapper class for bm25 vectorizer implemented on top of Pyserini.
+
+    Parameters
+    ----------
+    lucene_index_path : str
+        Path to lucene index folder
+    min_df : int
+        Minimum acceptable document frequency
+    verbose : bool
+        Whether to print out debugging information
+    """
+
+    def __init__(self, lucene_index_path: str, min_df: int = 1, verbose: bool = False) -> None:
+        super().__init__(lucene_index_path, verbose, min_df)
+
+    def get_vectors(self, doc_ids: List[str]):
+        """Get the bm25 vectors given a list of doc_ids
+
+        Parameters
+        ----------
+        doc_ids : List[str]
+            The piece of text to analyze.
+
+        Returns
+        -------
+        csr_matrix
+            L2 normalized sparse matrix representation of bm25 vectors
+        """
+        matrix_row, matrix_col, matrix_data = [], [], []
+        num_docs = len(doc_ids)
+
+        for index, doc_id in enumerate(doc_ids):
+            if index % 1000 == 0 and num_docs > 1000 and self.verbose:
+                print(f'Vectorizing: {index}/{len(doc_ids)}')
+
+            tf = self.index_utils.get_document_vector(doc_id)
+            tf = {term: self.index_utils.compute_bm25_term_weight(doc_id, term, analyzer=None) for term in tf.keys()}
+
+            # Convert from dict to sparse matrix
+            for term in tf:
+                matrix_row.append(index)
+                matrix_col.append(self.term_to_index[term])
+                matrix_data.append(tf[term])
+
+        vectors = csr_matrix((matrix_data, (matrix_row, matrix_col)), shape=(num_docs, self.vocabulary_size))
+        return self._l2normalize(vectors)
--- scripts/20newsgroups-replication.py
+++ scripts/20newsgroups-replication.py
@@ -1,9 +1,11 @@
 import sys
 sys.path.insert(0, './')
-
-from sklearn import metrics
-from sklearn.naive_bayes import MultinomialNB
+import argparse
 import os
+from enum import Enum
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from pyserini.vectorizer import BM25Vectorizer
 from pyserini.vectorizer import TfidfVectorizer
 
 
@@ -19,43 +21,44 @@ def get_info(path):
     return (docs, targets)
 
 
+class VectorizerType(Enum):
+    TFIDF = 'tfidf'
+    BM25 = 'bm25'
+
+
 if __name__ == '__main__':
-    target_names = [
-        'alt.atheism',
-        'comp.graphics',
-        'comp.os.ms-windows.misc',
-        'comp.sys.ibm.pc.hardware',
-        'comp.sys.mac.hardware',
-        'comp.windows.x',
-        'misc.forsale',
-        'rec.autos',
-        'rec.motorcycles',
-        'rec.sport.baseball',
-        'rec.sport.hockey',
-        'sci.crypt',
-        'sci.electronics',
-        'sci.med',
-        'sci.space',
-        'soc.religion.christian',
-        'talk.politics.guns',
-        'talk.politics.mideast',
-        'talk.politics.misc',
-        'talk.religion.misc',
-    ]
+    parser = argparse.ArgumentParser(
+        description='Replication script of pyserini vectorizer')
+    parser.add_argument('-vectorizer', type=VectorizerType, required=True, help='which vectorizer to use')
+    args = parser.parse_args()
+
+    target_names = ['alt.atheism', 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware', 'comp.windows.x', 'misc.forsale', 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball',
+                    'rec.sport.hockey', 'sci.crypt', 'sci.electronics', 'sci.med', 'sci.space', 'soc.religion.christian', 'talk.politics.guns', 'talk.politics.mideast', 'talk.politics.misc', 'talk.religion.misc', ]
 
     target_to_index = {t: i for i, t in enumerate(target_names)}
 
     train_docs, train_labels = get_info('./20newsgroups/20news-bydate-train/')
     test_docs, test_labels = get_info('./20newsgroups/20news-bydate-test/')
 
-    vectorizer = TfidfVectorizer(
-        './20newsgroups/lucene-index.20newsgroup.pos+docvectors+raw', min_df=5)
+    lucene_index_path = '20newsgroups/lucene-index.20newsgroup.pos+docvectors+raw'
+    vectorizer = None
+    if args.vectorizer == VectorizerType.TFIDF:
+        vectorizer = TfidfVectorizer(lucene_index_path, min_df=5, verbose=True)
+    elif args.vectorizer == VectorizerType.BM25:
+        vectorizer = BM25Vectorizer(lucene_index_path)
+
     train_vectors = vectorizer.get_vectors(train_docs)
     test_vectors = vectorizer.get_vectors(test_docs)
 
     # classifier
-    clf = MultinomialNB()
+    clf = LogisticRegression()
     clf.fit(train_vectors, train_labels)
     pred = clf.predict(test_vectors)
     score = metrics.f1_score(test_labels, pred, average='macro')
     print(f'f1 score: {score}')
+
+    score = round(score, 7)
+    if args.vectorizer == VectorizerType.TFIDF:
+        assert score == 0.8341188, "tf-idf vectorizer score mismatch"
+    elif args.vectorizer == VectorizerType.BM25:
+        assert score == 0.8441544, "bm25 vectorizer score mismatch"
