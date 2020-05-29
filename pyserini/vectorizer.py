a84945154045f0754ba7fcb9ff467965f9c4104b
Johnson Han <x65han@uwaterloo.ca>
2020-05-29 15:04:52 -0700

    Introduce new BM25Vectorizer (#149)
    * Introduce new BM25Vectorizer
    
    * Add VectorizerBase doc string
    
    * Add license information for scripts/20newsgroups-replication.py
    
    * Rename class VectorizerBase -> class Vectorizer
    
    * 20newsgroup replicaton adopt getattr() over Enum when retrieve vectorizer instance
    
    * Update f1 score after updating min_df=5
    
    * Vectorizer should consider when tf=None

--- pyserini/vectorizer.py
+++ pyserini/vectorizer.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-#
 # Pyserini: Python interface to the Anserini IR toolkit built on Lucene
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,7 +20,49 @@ from pyserini.search import pysearch
 from pyserini.index import pyutils
 
 
-class TfidfVectorizer:
+class Vectorizer:
+    """Base class for vectorizer implemented on top of Pyserini.
+
+    Parameters
+    ----------
+    lucene_index_path : str
+        Path to lucene index folder
+    min_df : int
+        Minimum acceptable document frequency
+    verbose : bool
+        Whether to print out debugging information
+    """
+
+    def __init__(self, lucene_index_path: str, min_df: int = 1, verbose: bool = False):
+        self.min_df: int = min_df
+        self.verbose: bool = verbose
+        self.index_utils = pyutils.IndexReaderUtils(lucene_index_path)
+        self.searcher = pysearch.SimpleSearcher(lucene_index_path)
+        self.num_docs: int = self.searcher.num_docs
+
+        # build vocabulary
+        self.vocabulary_ = set()
+        for term in self.index_utils.terms():
+            if term.df > self.min_df:
+                self.vocabulary_.add(term.term)
+
+        # build term to index mapping
+        self.term_to_index = {}
+        for index, term in enumerate(self.vocabulary_):
+            self.term_to_index[term] = index
+        self.vocabulary_size = len(self.vocabulary_)
+
+        if self.verbose:
+            print(f'Found {self.vocabulary_size} terms with min_df={self.min_df}')
+
+    def _l2normalize(self, a):
+        norm_rows = np.sqrt(np.add.reduceat(a.data * a.data, a.indptr[:-1]))
+        nnz_per_row = np.diff(a.indptr)
+        a.data /= np.repeat(norm_rows, nnz_per_row)
+        return a
+
+
+class TfidfVectorizer(Vectorizer):
     """Wrapper class for tf-idf vectorizer implemented on top of Pyserini.
 
     Parameters
@@ -35,44 +75,19 @@ class TfidfVectorizer:
         Whether to print out debugging information
     """
 
-    def __init__(self, lucene_index_path: str, min_df: int = 1, verbose: bool = False) -> None:
-        self.verbose: bool = verbose
-        self.min_df: int = min_df
-        self.index_utils = pyutils.IndexReaderUtils(lucene_index_path)
+    def __init__(self, lucene_index_path: str, min_df: int = 1, verbose: bool = False):
+        super().__init__(lucene_index_path, min_df, verbose)
 
-        # get num_docs
-        self.searcher = pysearch.SimpleSearcher(lucene_index_path)
-        self.num_docs: int = self.searcher.num_docs
-
-        # pre-processing
-        self.vocabulary_ = set()
         self.idf_ = {}
-
         for term in self.index_utils.terms():
             self.idf_[term.term] = math.log(self.num_docs / term.df)
-            if term.df > self.min_df:
-                self.vocabulary_.add(term.term)
 
-        self.term_to_index = {}
-        for index, term in enumerate(self.vocabulary_):
-            self.term_to_index[term] = index
-        self.vocabulary_size = len(self.vocabulary_)
-
-        if self.verbose:
-            print(f'Found {self.vocabulary_size} terms')
-
-    def l2norm(self, a):
-        norm_rows = np.sqrt(np.add.reduceat(a.data * a.data, a.indptr[:-1]))
-        nnz_per_row = np.diff(a.indptr)
-        a.data /= np.repeat(norm_rows, nnz_per_row)
-        return a
-
-    def get_vectors(self, doc_ids: List[str]):
-        """Get the tf-idf vectors given a list of doc_ids
+    def get_vectors(self, docids: List[str]):
+        """Get the tf-idf vectors given a list of docids
 
         Parameters
         ----------
-        doc_ids : List[str]
+        docids : List[str]
             The piece of text to analyze.
 
         Returns
@@ -81,11 +96,11 @@ class TfidfVectorizer:
             L2 normalized sparse matrix representation of tf-idf vectors
         """
         matrix_row, matrix_col, matrix_data = [], [], []
-        num_docs = len(doc_ids)
+        num_docs = len(docids)
 
-        for index, doc_id in enumerate(doc_ids):
+        for index, doc_id in enumerate(docids):
             if index % 1000 == 0 and num_docs > 1000 and self.verbose:
-                print(f'Vectorizing: {index}/{len(doc_ids)}')
+                print(f'Vectorizing: {index}/{len(docids)}')
 
             # Term Frequency
             tf = self.index_utils.get_document_vector(doc_id)
@@ -103,4 +118,59 @@ class TfidfVectorizer:
                 matrix_data.append(tfidf)
 
         vectors = csr_matrix((matrix_data, (matrix_row, matrix_col)), shape=(num_docs, self.vocabulary_size))
-        return self.l2norm(vectors)
+        return self._l2normalize(vectors)
+
+
+class BM25Vectorizer(Vectorizer):
+    """Wrapper class for BM25 vectorizer implemented on top of Pyserini.
+
+    Parameters
+    ----------
+    lucene_index_path : str
+        Path to lucene index folder
+    min_df : int
+        Minimum acceptable document frequency
+    verbose : bool
+        Whether to print out debugging information
+    """
+
+    def __init__(self, lucene_index_path: str, min_df: int = 1, verbose: bool = False):
+        super().__init__(lucene_index_path, min_df, verbose)
+
+    def get_vectors(self, docids: List[str]):
+        """Get the BM25 vectors given a list of docids
+
+        Parameters
+        ----------
+        docids : List[str]
+            The piece of text to analyze.
+
+        Returns
+        -------
+        csr_matrix
+            L2 normalized sparse matrix representation of BM25 vectors
+        """
+        matrix_row, matrix_col, matrix_data = [], [], []
+        num_docs = len(docids)
+
+        for index, doc_id in enumerate(docids):
+            if index % 1000 == 0 and num_docs > 1000 and self.verbose:
+                print(f'Vectorizing: {index}/{len(docids)}')
+
+            # Term Frequency
+            tf = self.index_utils.get_document_vector(doc_id)
+            if tf is None:
+                continue
+
+            # Filter out in-eligible terms
+            tf = {t: tf[t] for t in tf if t in self.term_to_index}
+
+            # Convert from dict to sparse matrix
+            for term in tf:
+                bm25_weight = self.index_utils.compute_bm25_term_weight(doc_id, term, analyzer=None)
+                matrix_row.append(index)
+                matrix_col.append(self.term_to_index[term])
+                matrix_data.append(bm25_weight)
+
+        vectors = csr_matrix((matrix_data, (matrix_row, matrix_col)), shape=(num_docs, self.vocabulary_size))
+        return self._l2normalize(vectors)
