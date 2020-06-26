4c8254e8ef5c49d56b4edc00a434b829ef2b9ef0
x65han <x65han@uwaterloo.ca>
2020-06-25 18:35:20 -0700

    Rename verify simple search to allow python.unittest to pick it up

--- integrations/test_simple_fusion_search_integration.py
+++ integrations/test_simple_fusion_search_integration.py
@@ -25,19 +25,19 @@ from pyserini.search import get_topics, SimpleFusionSearcher, SimpleSearcher
 
 class TestSearchIntegration(unittest.TestCase):
     def setUp(self):
-        if not os.path.exists('lucene-index-cord19-abstract-2020-05-01/'):
+        if not os.path.exists('indexes/lucene-index-cord19-abstract-2020-05-01/'):
             os.system('wget -nc https://www.dropbox.com/s/wxjoe4g71zt5za2/lucene-index-cord19-abstract-2020-05-01.tar.gz')
-            os.system('tar -xvzf lucene-index-cord19-abstract-2020-05-01.tar.gz')
+            os.system('tar -xvzf lucene-index-cord19-abstract-2020-05-01.tar.gz -C indexes')
             os.system('rm lucene-index-cord19-abstract-2020-05-01.tar.gz')
 
-        if not os.path.exists('lucene-index-cord19-full-text-2020-05-01/'):
+        if not os.path.exists('indexes/lucene-index-cord19-full-text-2020-05-01/'):
             os.system('wget -nc https://www.dropbox.com/s/di27r5o2g5kat5k/lucene-index-cord19-full-text-2020-05-01.tar.gz')
-            os.system('tar -xvzf lucene-index-cord19-full-text-2020-05-01.tar.gz')
+            os.system('tar -xvzf lucene-index-cord19-full-text-2020-05-01.tar.gz -C indexes')
             os.system('rm lucene-index-cord19-full-text-2020-05-01.tar.gz')
 
-        if not os.path.exists('lucene-index-cord19-paragraph-2020-05-01/'):
+        if not os.path.exists('indexes/lucene-index-cord19-paragraph-2020-05-01/'):
             os.system('wget -nc https://www.dropbox.com/s/6ib71scm925mclk/lucene-index-cord19-paragraph-2020-05-01.tar.gz')
-            os.system('tar -xvzf lucene-index-cord19-paragraph-2020-05-01.tar.gz')
+            os.system('tar -xvzf lucene-index-cord19-paragraph-2020-05-01.tar.gz -C indexes')
             os.system('rm lucene-index-cord19-paragraph-2020-05-01.tar.gz')
 
         if not os.path.exists('anserini.covid-r2.fusion1.txt'):
@@ -45,29 +45,28 @@ class TestSearchIntegration(unittest.TestCase):
             os.system('gunzip -f anserini.covid-r2.fusion1.txt.gz')
 
     def test_simple_fusion_searcher(self):
-        # index_dirs = ['lucene-index-cord19-abstract-2020-05-01/',
-        #               'lucene-index-cord19-full-text-2020-05-01/',
-        #               'lucene-index-cord19-paragraph-2020-05-01/']
+        index_dirs = ['indexes/lucene-index-cord19-abstract-2020-05-01/',
+                      'indexes/lucene-index-cord19-full-text-2020-05-01/',
+                      'indexes/lucene-index-cord19-paragraph-2020-05-01/']
 
-        # searcher = SimpleFusionSearcher(index_dirs, method=FusionMethod.RRF)
+        searcher = SimpleFusionSearcher(index_dirs, method=FusionMethod.RRF)
 
-        # runs, topics = [], get_topics('covid_round2')
-        # for topic in tqdm(sorted(topics.keys())):
-        #     query = topics[topic]['question'] + ' ' + topics[topic]['query']
-        #     hits = searcher.search(query, k=10000, query_generator=None, strip_segment_id=True, remove_dups=True)
-        #     docid_score_pair = [(hit.docid, hit.score) for hit in hits]
-        #     run = TrecRun.from_search_results(docid_score_pair, topic=topic)
-        #     runs.append(run)
+        runs, topics = [], get_topics('covid_round2')
+        for topic in tqdm(sorted(topics.keys())):
+            query = topics[topic]['question'] + ' ' + topics[topic]['query']
+            hits = searcher.search(query, k=10000, query_generator=None, strip_segment_id=True, remove_dups=True)
+            docid_score_pair = [(hit.docid, hit.score) for hit in hits]
+            run = TrecRun.from_search_results(docid_score_pair, topic=topic)
+            runs.append(run)
 
-        # all_topics_run = TrecRun.concat(runs)
-        # all_topics_run.save_to_txt(output_path='fused.txt', tag='reciprocal_rank_fusion_k=60')
+        all_topics_run = TrecRun.concat(runs)
+        all_topics_run.save_to_txt(output_path='fused.txt', tag='reciprocal_rank_fusion_k=60')
 
-        # # Only keep topic, docid and rank. Scores have different floating point precisions.
-        # os.system("""awk '{print $1" "$3" "$4}' fused.txt > this.txt""")
-        # os.system("""awk '{print $1" "$3" "$4}' anserini.covid-r2.fusion1.txt > that.txt""")
+        # Only keep topic, docid and rank. Scores have different floating point precisions.
+        os.system("""awk '{print $1" "$3" "$4}' fused.txt > this.txt""")
+        os.system("""awk '{print $1" "$3" "$4}' anserini.covid-r2.fusion1.txt > that.txt""")
 
-        # self.assertTrue(filecmp.cmp('this.txt', 'that.txt'))
-        self.assertTrue(1==1)
+        self.assertTrue(filecmp.cmp('this.txt', 'that.txt'))
 
     def tearDown(self):
         os.system('rm anserini.covid-r2.fusion1.txt fused.txt this.txt that.txt')
