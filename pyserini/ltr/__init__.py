ca2de6493f7e6034b75e48cf74f4fd28ff56c187
nsndimt <nsndimt@gmail.com>
2020-08-18 15:10:40 -0400

    fix error; refactor how to config extractor

--- pyserini/ltr/__init__.py
+++ pyserini/ltr/__init__.py
@@ -1,3 +1,3 @@
-from ._base import FeatureExtractor
+from ._base import FeatureExtractor, QueryLength, BM25
 
-__all__ = ['FeatureExtractor']
+__all__ = ['FeatureExtractor','QueryLength','BM25']
--- pyserini/ltr/_base.py
+++ pyserini/ltr/_base.py
@@ -1,18 +1,22 @@
 from ..pyclass import autoclass, JString, JArrayList
 
-JFeatureExtractor = autoclass('io.anserini.ltr.FeatureExtractorUtils')
-JQueryLength = autoclass('io.anserini.ltr.feature.base.QueryLength')
-JBM25FeatureExtractor = autoclass('io.anserini.ltr.feature.base.BM25FeatureExtractor')
+class QueryLength:
+    def __init__(self):
+        JQueryLength = autoclass('io.anserini.ltr.feature.base.QueryLength')
+        self.extractor = JQueryLength()
+
+class BM25:
+    def __init__(self, k1=0.9, b=0.4):
+        JBM25FeatureExtractor = autoclass('io.anserini.ltr.feature.base.BM25FeatureExtractor')
+        self.extractor = JBM25FeatureExtractor(k1, b)
 
 class FeatureExtractor:
-    def __init__(self, index_dir, config):
-        self.extractor = JFeatureExtractor(JString(index_dir))
-        if 'QueryLength' in config:
-            self.extractor.add(JQueryLength())
-        if 'BM25' in config:
-            k1 = config['BM25']['k1']
-            b = config['BM25']['b']
-            self.extractor.add(JBM25FeatureExtractor(k1, b))
+    def __init__(self, index_dir):
+        JFeatureExtractorUtils = autoclass('io.anserini.ltr.FeatureExtractorUtils')
+        self.utils = JFeatureExtractorUtils(JString(index_dir))
+
+    def add(self, pyclass):
+        self.utils.add(pyclass.extractor)
 
     def extract(self, query_tokens, doc_ids):
         queryTokens = JArrayList()
@@ -21,10 +25,10 @@ class FeatureExtractor:
         docIds = JArrayList()
         for did in doc_ids:
             docIds.add(JString(did))
-        res = self.extractor.extract(queryTokens, docIds)
+        res = self.utils.extract(queryTokens, docIds)
         features = {}
         for did in res.keySet().toArray():
-            features[did] = res.get(JString('FBIS3-1')).toArray()
+            features[did] = res.get(JString(did)).toArray()
         return features
 
 
