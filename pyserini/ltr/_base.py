ccf05ecc92b8fea95cec3d962c3158074f152406
nsndimt <nsndimt@gmail.com>
2020-08-18 16:53:31 -0400

    remember feature order

--- pyserini/ltr/_base.py
+++ pyserini/ltr/_base.py
@@ -5,18 +5,29 @@ class QueryLength:
         JQueryLength = autoclass('io.anserini.ltr.feature.base.QueryLength')
         self.extractor = JQueryLength()
 
+    def name(self):
+        return 'query_len'
+
 class BM25:
     def __init__(self, k1=0.9, b=0.4):
         JBM25FeatureExtractor = autoclass('io.anserini.ltr.feature.base.BM25FeatureExtractor')
         self.extractor = JBM25FeatureExtractor(k1, b)
 
+    def name(self):
+        return 'bm25'
+
 class FeatureExtractor:
     def __init__(self, index_dir):
         JFeatureExtractorUtils = autoclass('io.anserini.ltr.FeatureExtractorUtils')
         self.utils = JFeatureExtractorUtils(JString(index_dir))
+        self.feature_name = []
 
     def add(self, pyclass):
         self.utils.add(pyclass.extractor)
+        self.feature_name.append(pyclass.name())
+
+    def feature_names(self):
+        return self.feature_name
 
     def extract(self, query_tokens, doc_ids):
         queryTokens = JArrayList()
