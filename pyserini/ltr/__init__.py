d3cd480394432ec7c5657fa18064f35a327b8115
nsndimt <nsndimt@gmail.com>
2020-10-25 18:31:29 -0400

    fix import problem

--- pyserini/ltr/__init__.py
+++ pyserini/ltr/__init__.py
@@ -1,10 +1,11 @@
-from ._base import FeatureExtractor, AvgICTF, AvgIDF, BM25, LMDir, DFR_GL2, DFR_In_expB2, \
+from ._base import FeatureExtractor, AvgICTF, AvgIDF, BM25, LMDir, LMJM, DFR_GL2, DFR_In_expB2, \
     DocSize, MatchingTermCount, QueryLength, AvgSCQ, SCS, SumMatchingTF, UniqueTermCount, \
     UnorderedSequentialPairs, OrderedSequentialPairs, UnorderedQueryPairs, OrderedQueryPairs, \
-    AvgPooler, SumPooler, MedianPooler, MinPooler, MaxPooler, VarPooler, tfStat, tfIdfStat, normalizedTfStat
+    AvgPooler, SumPooler, MedianPooler, MinPooler, MaxPooler, VarPooler, tfStat, tfIdfStat, normalizedTfStat, \
+    idfStat, ictfStat, scqStat
 
-__all__ = ['FeatureExtractor', 'AvgICTF', 'AvgIDF', 'BM25', 'LMDir', 'DFR_GL2', 'DFR_In_expB2',
+__all__ = ['FeatureExtractor', 'AvgICTF', 'AvgIDF', 'BM25', 'LMDir', 'LMJM','DFR_GL2', 'DFR_In_expB2',
            'DocSize', 'MatchingTermCount', 'QueryLength', 'AvgSCQ', 'SCS', 'SumMatchingTF', 'UniqueTermCount',
            'UnorderedSequentialPairs', 'OrderedSequentialPairs', 'UnorderedQueryPairs', 'OrderedQueryPairs',
            'AvgPooler', 'SumPooler', 'MedianPooler', 'MinPooler', 'MaxPooler', 'VarPooler', 'tfStat', 'tfIdfStat',
-           'normalizedTfStat']
+           'normalizedTfStat','idfStat', 'ictfStat', 'scqStat']
