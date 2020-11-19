f5d4e669877467f7c02d05a392d617348e01eef2
lintool <jimmylin@uwaterloo.ca>
2020-11-19 13:56:37 -0500

    Index download bugfix.

--- pyserini/util.py
+++ pyserini/util.py
@@ -89,6 +89,7 @@ def get_cache_home():
 def download_and_unpack_index(url, index_directory='indexes', force=False, verbose=True, prebuilt=False, md5=None):
     index_name = url.split('/')[-1]
     index_name = re.sub('''.tar.gz.*$''', '', index_name)
+    local_tarball = os.path.join(index_directory, f'{index_name}.tar.gz')
 
     if prebuilt:
         index_directory = os.path.join(get_cache_home(), 'indexes')
@@ -96,10 +97,6 @@ def download_and_unpack_index(url, index_directory='indexes', force=False, verbo
 
         if not os.path.exists(index_directory):
             os.makedirs(index_directory)
-
-        local_tarball = os.path.join(index_directory, f'{index_name}.tar.gz')
-        if os.path.exists(local_tarball):
-            os.remove(local_tarball)
     else:
         index_path = os.path.join(index_directory, f'{index_name}')
 
@@ -123,8 +120,10 @@ def download_and_unpack_index(url, index_directory='indexes', force=False, verbo
     tarball.extractall(index_directory)
     tarball.close()
     os.remove(local_tarball)
+
     if prebuilt:
         os.rename(os.path.join(index_directory, f'{index_name}'), index_path)
+
     return index_path
 
 
