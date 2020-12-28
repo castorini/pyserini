cb8ada0236647957e8bb3bf6e63c873c0398f596
Yuqi <yuqiliu899@gmail.com>
2020-12-28 14:25:52 -0500

    merge ltr

--- pyserini/ltr/__init__.py
+++ pyserini/ltr/__init__.py
@@ -12,5 +12,5 @@ __all__ = ['FeatureExtractor', 'BM25', 'LMDir', 'LMJM','DFR_GL2', 'DFR_In_expB2'
            'AvgPooler', 'SumPooler', 'MedianPooler', 'MinPooler', 'MaxPooler', 'VarPooler', 'tfStat', 'tfIdfStat',
            'normalizedTfStat','idfStat', 'ictfStat', 'scqStat','ContextDFR_GL2', 'ContextDPH', 'ContextDFR_In_expB2',
            'ConfidencePooler', 'MaxMinRatioPooler','NTFIDF', 'normalizedDocSizeStat', 'Entropy', 'StopCover',
-           'StopRatio', 'SDM', 'QueryLengthNonStopWords', 'ProbalitySum', 'RunList','BM25Conf', 'BM25Min', 'BM25Max', 
-           'BM25Mean', 'BM25HMean', 'BM25Var', 'BM25Quartile', 'IBMModel1']
+           'StopRatio', 'SDM', 'QueryLengthNonStopWords', 'ProbalitySum', 'RunList','BM25Conf', 'BM25HMean', 'BM25Max', 
+           'BM25Mean', 'BM25Min', 'BM25Quartile', 'BM25Var']
