1018715cf629e3dd98d1c47e906841e8ea89c0ec
nsndimt <nsndimt@gmail.com>
2021-01-04 11:49:31 -0500

    add missing howmany

--- pyserini/ltr/__init__.py
+++ pyserini/ltr/__init__.py
@@ -14,5 +14,5 @@ __all__ = ['FeatureExtractor', 'BM25', 'LMDir', 'LMJM','DFR_GL2', 'DFR_In_expB2'
            'normalizedTfStat','idfStat', 'ictfStat', 'scqStat','ContextDFR_GL2', 'ContextDPH', 'ContextDFR_In_expB2',
            'ConfidencePooler', 'MaxMinRatioPooler','NTFIDF', 'normalizedDocSizeStat', 'Entropy', 'StopCover',
            'StopRatio', 'SDM', 'ProbalitySum', 'RunList','BM25Min', 'BM25Max',
-           'BM25Mean', 'BM25HMean', 'BM25Var', 'BM25Quartile', 'IBMModel1', 'EntityHowMuch', 'EntityHowLong', 'EntityWho',
-           'EntityWhere', 'EntityWhen', 'EntityWhereMatch', 'EntityWhoMatch']
+           'BM25Mean', 'BM25HMean', 'BM25Var', 'BM25Quartile', 'IBMModel1', 'EntityHowMany','EntityHowMuch',
+           'EntityHowLong', 'EntityWho','EntityWhere', 'EntityWhen', 'EntityWhereMatch', 'EntityWhoMatch']
