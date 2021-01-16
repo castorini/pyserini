37cbfd8cd453342777e49d8ee06399eb7fbfba47
Xueguang Ma 马雪光 <x93ma@edu.uwaterloo.ca>
2021-01-15 21:50:26 -0500

    update re pickled query info (#322)
    recreate pickle for encoded queries from Py3.6

--- pyserini/encoded_query_info.py
+++ pyserini/encoded_query_info.py
@@ -2,10 +2,10 @@ QUERY_INFO = {
     "msmarco-passage-dev-subset-tct_colbert": {
         "description": "MS MARCO passage dev set queries encoded by TCT-ColBERT",
         "urls": [
-            "https://www.dropbox.com/s/l5ycxuzumi7fkl6/query-embedding-msmarco-passage-dev-subset-20210112-be7119.tar.gz?dl=1",
+            "https://www.dropbox.com/s/g7sci7n15mekbut/query-embedding-msmarco-passage-dev-subset-20210115-cd5034.tar.gz?dl=1",
         ],
-        "md5": "c9398a713ff13e1780ea2aa2747deb2f",
-        "size (bytes)": 20074204,
+        "md5": "0e448fda77344e7b892e861f416a8870",
+        "size (bytes)": 20075071,
         "total_queries": 6980,
         "downloaded": False
     }
