6c4dd449e258bf36bf045eaa86e2c12ac4e73916
Xueguang Ma <x93ma@edu.uwaterloo.ca>
2021-01-19 01:04:25 -0500

    update script and pypi doc

--- pyserini/eval/msmarco_doc_eval.py
+++ pyserini/eval/msmarco_doc_eval.py
@@ -1,12 +1,21 @@
+import os
 import subprocess
 import sys
 
+from pyserini.search import get_qrels
 from pyserini.util import download_evaluation_script
 
 script_path = download_evaluation_script('msmarco_doc_eval')
 cmd_prefix = ['python3', script_path]
 args = sys.argv
-cmd = cmd_prefix + args[1:] if len(args) > 1 else cmd_prefix
+if len(args) > 1:
+    cmd = cmd_prefix + args[1:]
+    for i in range(len(cmd)-1):
+        if cmd[i] == '--judgments':
+            if not os.path.exists(cmd[i+1]):
+                cmd[i+1] = get_qrels(cmd[i+1])
+else:
+    cmd = cmd_prefix
 print(f'Running command: {cmd}')
 process = subprocess.Popen(cmd,
                            stdout=subprocess.PIPE,
--- pyserini/eval/msmarco_passage_eval.py
+++ pyserini/eval/msmarco_passage_eval.py
@@ -1,12 +1,19 @@
+import os
 import subprocess
 import sys
 
+from pyserini.search import get_qrels
 from pyserini.util import download_evaluation_script
 
 script_path = download_evaluation_script('msmarco_passage_eval')
 cmd_prefix = ['python3', script_path]
 args = sys.argv
-cmd = cmd_prefix + args[1:] if len(args) > 1 else cmd_prefix
+if len(args) > 1:
+    cmd = cmd_prefix + args[1:]
+    if not os.path.exists(cmd[-2]):
+        cmd[-2] = get_qrels(cmd[-2])
+else:
+    cmd = cmd_prefix
 print(f'Running command: {cmd}')
 process = subprocess.Popen(cmd,
                            stdout=subprocess.PIPE,
