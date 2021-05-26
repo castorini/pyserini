b655b50129934748ee72b6b4ca5d4c567633e4d2
x93ma@edu.uwaterloo.ca <x93ma@edu.uwaterloo.ca>
2021-05-26 19:18:16 -0400

    add dense index creat doc

---
+++ scripts/merge_indexes.py
@@ -0,0 +1,25 @@
+import argparse
+
+import faiss
+import os
+import shutil
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dimension', type=int, help='dimension of passage embeddings', required=False, default=768)
+parser.add_argument('--prefix', type=str, help='directory to store brute force index of corpus', required=True)
+parser.add_argument('--shard-num', type=int, help='number of shards', default=1)
+args = parser.parse_args()
+
+new_index = faiss.IndexFlatIP(args.dimension)
+docid_files = []
+for i in range(args.shard_num):
+    index = faiss.read_index(os.path.join(args.prefix + str(i), 'index'))
+    docid_files.append(os.path.join(args.prefix + str(i), 'docid'))
+    vectors = index.reconstruct_n(0, index.ntotal)
+    new_index.add(vectors)
+faiss.write_index(new_index, os.path.join(args.prefix + 'full', 'index'))
+
+with open(os.path.join(args.prefix + 'full', 'docid'), 'wb') as wfd:
+    for f in docid_files:
+        with open(f, 'rb') as fd:
+            shutil.copyfileobj(fd, wfd)
