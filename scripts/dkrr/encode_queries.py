294786b4f9e8856c69fc7d3411fdc1d64c638ecc
ronakice <ronakice@hotmail.com>
2021-06-02 16:25:47 +0530

    add query encoding script

--- scripts/dkrr/encode_queries.py
+++ scripts/dkrr/encode_queries.py
@@ -15,65 +15,59 @@
 #
 
 import argparse
-import csv
-import json
 
 import pandas as pd
+
 from tqdm import tqdm
-from transformers import DPRQuestionEncoder, DPRQuestionEncoderTokenizer
+from pyserini.query_iterator import get_query_iterator, TopicsFormat
+from transformers import BertModel,  BertTokenizerFast
+import torch
 
+class DkrrDprQueryEncoder():
 
-def parse_qa_csv_file(location):
-    with open(location) as file:
-        reader = csv.reader(file, delimiter='\t')
-        for row in reader:
-            question = row[0]
-            answers = eval(row[1])
-            yield question, answers
+    def __init__(self, encoder: str = None, device: str = 'cpu', prefix: str = "question:"):
+        self.device = device
+        self.model = BertModel.from_pretrained(encoder)
+        self.model.to(self.device)
+        self.tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+        self.prefix = prefix
 
+    @staticmethod
+    def _mean_pooling(model_output, attention_mask):
+        model_output = model_output[0].masked_fill(1 - attention_mask[:, :, None], 0.)
+        model_output = torch.sum(model_output, dim=1) / torch.clamp(torch.sum(attention_mask, dim=1), min=1e-9)[:, None]
+        return model_output.flatten()
 
-def parse_qa_json_file(location):
-    with open(location) as file:
-        for line in file:
-            qa = json.loads(line)
-            question = qa['question']
-            answers = qa['answer']
-            yield question, answers
-
-
-def encode_query(text, tokenizer, model, device='cpu'):
-    input_ids = tokenizer(text, return_tensors='pt')
-    input_ids.to(device)
-    embeddings = model(input_ids["input_ids"]).pooler_output.detach().cpu().numpy()
-    return embeddings.flatten()
-
+    def encode(self, query: str):
+        if self.prefix:
+            query = f'{self.prefix} {query}'
+        inputs = self.tokenizer(query, return_tensors='pt', max_length=40, padding="max_length")
+        inputs.to(self.device)
+        outputs = self.model(input_ids=inputs["input_ids"],
+                            attention_mask=inputs["attention_mask"])
+        embeddings = self._mean_pooling(outputs, inputs['attention_mask']).detach().cpu().numpy()
+        return embeddings.flatten()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--topics', type=str, metavar='topic_name', required=True,
+                        help="Name of topics.")
     parser.add_argument('--encoder', type=str, help='encoder name or path',
                         default='facebook/dpr-question_encoder-multiset-base', required=False)
-    parser.add_argument('--input', type=str, help='qas file, json file by default', required=True)
-    parser.add_argument('--format', type=str, help='qas file format', default='json', required=False)
     parser.add_argument('--output', type=str, help='path to store query embeddings', required=True)
     parser.add_argument('--device', type=str,
                         help='device cpu or cuda [cuda:0, cuda:1...]', default='cpu', required=False)
     args = parser.parse_args()
 
-    tokenizer = DPRQuestionEncoderTokenizer.from_pretrained(args.encoder)
-    model = DPRQuestionEncoder.from_pretrained(args.encoder)
-    model.to(args.device)
+    query_iterator = get_query_iterator(args.topics, TopicsFormat(TopicsFormat.DEFAULT.value))
+    topics = query_iterator.topics
+    
+    encoder = DkrrDprQueryEncoder(args.encoder, args.device)
 
     embeddings = {'id': [], 'text': [], 'embedding': []}
-    qa_parser = None
-    if args.format == 'csv':
-        qa_parser = parse_qa_csv_file
-    elif args.format == 'json':
-        qa_parser = parse_qa_json_file
-    if qa_parser is None:
-        print(f'No QA parser defined for file format: {args.format}, or format not match')
-    for qid, (question, answers) in enumerate(tqdm(list(qa_parser(args.input)))):
-        embeddings['id'].append(qid)
-        embeddings['text'].append(question)
-        embeddings['embedding'].append(encode_query(question, tokenizer, model, args.device))
+    for index, (topic_id, text) in enumerate(tqdm(query_iterator, total=len(topics.keys()))):
+        embeddings['id'].append(topic_id)
+        embeddings['text'].append(text)
+        embeddings['embedding'].append(encoder.encode(text))
     embeddings = pd.DataFrame(embeddings)
     embeddings.to_pickle(args.output)
