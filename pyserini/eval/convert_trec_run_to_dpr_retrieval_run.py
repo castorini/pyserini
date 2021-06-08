b5ed46cf1118ba65fe2fe5faa57e992e9e91f1db
x93ma@edu.uwaterloo.ca <x93ma@edu.uwaterloo.ca>
2021-06-08 12:53:00 -0400

    update dpr eval

--- pyserini/eval/convert_trec_run_to_dpr_retrieval_run.py
+++ pyserini/eval/convert_trec_run_to_dpr_retrieval_run.py
@@ -20,12 +20,15 @@ import os
 from tqdm import tqdm
 
 from pyserini.search import SimpleSearcher, get_topics
+from pyserini.eval.evaluate_dpr_retrieval import has_answers, SimpleTokenizer
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert an TREC run to DPR retrieval result json.')
     parser.add_argument('--topics', required=True, help='topic name')
     parser.add_argument('--index', required=True, help='Anserini Index that contains raw')
     parser.add_argument('--input', required=True, help='Input TREC run file.')
+    parser.add_argument('--store-raw', action='store_true', help='Store raw text of passage')
+    parser.add_argument('--regex', action='store_true', default=False, help="regex match")
     parser.add_argument('--output', required=True, help='Output DPR Retrieval json file.')
     args = parser.parse_args()
 
@@ -39,6 +42,7 @@ if __name__ == '__main__':
         exit()
 
     retrieval = {}
+    tokenizer = SimpleTokenizer()
     with open(args.input) as f_in:
         for line in tqdm(f_in.readlines()):
             question_id, _, doc_id, _, score, _ = line.strip().split()
@@ -51,8 +55,18 @@ if __name__ == '__main__':
             ctx = json.loads(searcher.doc(doc_id).raw())['contents']
             if question_id not in retrieval:
                 retrieval[question_id] = {'question': question, 'answers': answers, 'contexts': []}
-            retrieval[question_id]['contexts'].append(
-                {'docid': doc_id, 'score': score, 'text': ctx}
-            )
+            title, text = ctx.split('\n')[1]
+            answer_exist = has_answers(text, answers, tokenizer, args.regex)
+            if args.store_raw:
+                retrieval[question_id]['contexts'].append(
+                    {'docid': doc_id,
+                     'score': score,
+                     'text': ctx,
+                     'has_answer': answer_exist}
+                )
+            else:
+                retrieval[question_id]['contexts'].append(
+                    {'docid': doc_id, 'score': score, 'has_answer': answer_exist}
+                )
 
     json.dump(retrieval, open(args.output, 'w'), indent=4)
