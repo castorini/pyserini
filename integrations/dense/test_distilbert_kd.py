246f80cc9ecb5a38772d92773c6c8dfebbf11bde
x93ma@edu.uwaterloo.ca <x93ma@edu.uwaterloo.ca>
2021-09-26 13:12:02 -0400

    fix test error

--- integrations/dense/test_distilbert_kd.py
+++ integrations/dense/test_distilbert_kd.py
@@ -51,7 +51,6 @@ class TestSearchIntegration(unittest.TestCase):
         stdout, stderr = run_command(cmd2)
         score = parse_score(stdout, "MRR @10")
         self.assertEqual(status, 0)
-        self.assertEqual(stderr, '')
         self.assertAlmostEqual(score, 0.3250, delta=0.0001)
 
     def test_msmarco_passage_distilbert_kd_encoded_queries(self):
