585a5a413da1b733a24656ad2c5329876c0c773a
lintool <jimmylin@uwaterloo.ca>
2021-10-23 13:28:20 -0400

    tweaks

--- pyserini/prebuilt_index_info.py
+++ pyserini/prebuilt_index_info.py
@@ -718,7 +718,8 @@ FAISS_INDEX_INFO = {
         "filename": "dindex-msmarco-passage-distilbert-dot-tas_b-b256-bf-20210527-63276f.tar.gz",
         "urls": [
             "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/dindex-msmarco-passage-distilbert-dot-tas_b-b256-bf-20210527-63276f.tar.gz",
-            "https://www.dropbox.com/s/1iu2wo2da1ai7ca/dindex-msmarco-passage-distilbert-dot-tas_b-b256-bf-20210527-63276f.tar.gz?dl=1",
+            # This link seems to be broken
+            #"https://www.dropbox.com/s/1iu2wo2da1ai7ca/dindex-msmarco-passage-distilbert-dot-tas_b-b256-bf-20210527-63276f.tar.gz?dl=1",
         ],
         "md5": "cc947bf66d9552a2a7c6fe060466e490",
         "size compressed (bytes)": 25162328596,
