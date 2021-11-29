17e2b516482bc29665c30bd3bc13732d49b826ff
Yuqi Liu <y899liu@uwaterloo.ca>
2021-11-29 22:59:02 +0800

    update with python name convention

--- scripts/rank_ibm.py
+++ scripts/rank_ibm.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 import argparse
 import os
 import json
@@ -32,10 +31,10 @@ JSimpleSearcher = autoclass('io.anserini.search.SimpleSearcher')
 JIndexReader = autoclass('io.anserini.index.IndexReaderUtils')
 JTerm = autoclass('org.apache.lucene.index.Term')
 
-selfTrans = 0.35
-minProb=0.0025
-lambdaValue = 0.3
-minCollectProb=1e-9
+SELF_TRAN = 0.35
+MIN_PROB=0.0025
+LAMBDA_VALUE = 0.3
+MIN_COLLECT_PROB=1e-9
 
 def normalize(scores: List[float]):
     low = min(scores)
@@ -105,42 +104,42 @@ def get_ibm_score(arguments):
     query_text_lst = arguments['query_text_lst']
     test_doc = arguments['test_doc']
     searcher = arguments['searcher']
-    fieldName = arguments['fieldName']
-    sourceLookup = arguments['sourceLookup']
-    targetLookup = arguments['targetLookup']
+    field_name = arguments['field_name']
+    source_lookup = arguments['source_lookup']
+    target_lookup = arguments['target_lookup']
     tran = arguments['tran']
-    collectProbs = arguments['collectProbs']
+    collect_probs = arguments['collect_probs']
 
     if searcher.documentRaw(test_doc) ==None:
         print(f'{test_doc} is not found in searcher')
-    document_text= json.loads(searcher.documentRaw(test_doc))[fieldName]
+    document_text= json.loads(searcher.documentRaw(test_doc))[field_name]
     doc_token_lst  = document_text.split(" ")
-    totalQueryProb = 0
-    docSize = len(doc_token_lst)
-    querySize = len(query_text_lst)
+    total_query_prob = 0
+    doc_size = len(doc_token_lst)
+    query_size = len(query_text_lst)
     for querytoken in query_text_lst:
-        targetMap = {}
-        totTranProb = 0
-        collectProb = collectProbs[querytoken]
-        if querytoken in targetLookup.keys():
-            queryWordId = targetLookup[querytoken]
-            if queryWordId in tran.keys():
-                targetMap = tran[queryWordId]
+        target_map = {}
+        total_tran_prob = 0
+        collect_prob = collect_probs[querytoken]
+        if querytoken in target_lookup.keys():
+            query_word_id = target_lookup[querytoken]
+            if query_word_id in tran.keys():
+                target_map = tran[query_word_id]
                 for doctoken in doc_token_lst:
-                    tranProb = 0
-                    docWordId = 0
+                    tran_prob = 0
+                    doc_word_id = 0
                     if querytoken==doctoken:
-                        tranProb = selfTrans  
-                    if doctoken in sourceLookup.keys():
-                        docWordId = sourceLookup[doctoken] 
-                        if docWordId in targetMap.keys():
-                            tranProb = max(targetMap[docWordId],tranProb)
-                            totTranProb += (tranProb/docSize)
+                        tran_prob = SELF_TRAN  
+                    if doctoken in source_lookup.keys():
+                        doc_word_id = source_lookup[doctoken] 
+                        if doc_word_id in target_map.keys():
+                            tran_prob = max(target_map[doc_word_id],tran_prob)
+                            total_tran_prob += (tran_prob/doc_size)
 
-        queryWordProb=math.log((1 - lambdaValue) * totTranProb + lambdaValue * collectProb) 
+        query_word_prob=math.log((1 - LAMBDA_VALUE) * total_tran_prob + LAMBDA_VALUE * collect_prob) 
 
-        totalQueryProb += queryWordProb
-    return totalQueryProb /querySize
+        total_query_prob += query_word_prob
+    return total_query_prob /query_size
 
 
 
@@ -158,86 +157,86 @@ def query_loader(query_path: str):
     return queries
 
 
-def intBitsToFloat(b: bytes):
+def intbits_to_float(b: bytes):
    s = struct.pack('>l', b)
    return struct.unpack('>f', s)[0]
 
-def rescale(sourceLookup: Dict[str,int],targetLookup: Dict[str,int],tranLookup: Dict[str,Dict[str,float]],\
-            targetVoc: Dict[int,str],sourceVoc: Dict[int,str]):
-    for targetID in tranLookup:
-        targetProbs = tranLookup[targetID]
-        if targetID > 0:
-            adjustMult = (1 - selfTrans) 
+def rescale(source_lookup: Dict[str,int],target_lookup: Dict[str,int],tran_lookup: Dict[str,Dict[str,float]],\
+            target_voc: Dict[int,str],source_voc: Dict[int,str]):
+    for target_id in tran_lookup:
+        target_probs = tran_lookup[target_id]
+        if target_id > 0:
+            adjust_mult = (1 - SELF_TRAN) 
         else:
-            adjustMult = 1
-        #adjust the prob with adjustMult and add selfTran prob to self-translation pair
-        for sourceID in targetProbs.keys():
-            tranProb = targetProbs[sourceID]
-            if sourceID >0:
-                sourceWord = sourceVoc[sourceID]
-                targetWord = targetVoc[targetID]
-                tranProb *= adjustMult
-                if (sourceWord== targetWord):
-                    tranProb += selfTrans
-                targetProbs[sourceID]= tranProb
+            adjust_mult = 1
+        #adjust the prob with adjust_mult and add SELF_TRAN prob to self-translation pair
+        for source_id in target_probs.keys():
+            tran_prob = target_probs[source_id]
+            if source_id >0:
+                source_word = source_voc[source_id]
+                target_word = target_voc[target_id]
+                tran_prob *= adjust_mult
+                if (source_word== target_word):
+                    tran_prob += SELF_TRAN
+                target_probs[source_id]= tran_prob
         # in case if self-translation pair was not included in TransTable
-        if targetID not in targetProbs.keys():
-            targetProbs[targetID]= selfTrans
-    return sourceLookup,targetLookup,tranLookup
+        if target_id not in target_probs.keys():
+            target_probs[target_id]= SELF_TRAN
+    return source_lookup,target_lookup,tran_lookup
 
 
 
-def load_tranProbsTable(dir_path: str):
+def load_tranprobs_table(dir_path: str):
     source_path = dir_path +"/source.vcb"
-    sourceLookup = {}
-    sourceVoc={}
+    source_lookup = {}
+    source_voc={}
     with open(source_path) as f:
         lines = f.readlines()
     for line in lines:
         id, voc,freq = line.split(" ")
-        sourceVoc[int(id)] = voc
-        sourceLookup[voc]=int(id)
+        source_voc[int(id)] = voc
+        source_lookup[voc]=int(id)
 
     target_path = dir_path +"/target.vcb"
-    targetLookup = {}
-    targetVoc = {}
+    target_lookup = {}
+    target_voc = {}
     with open(target_path) as f:
         lines = f.readlines()
     for line in lines:
         id, voc,freq = line.split(" ")
-        targetVoc[int(id)] = voc
-        targetLookup[voc]=int(id)
+        target_voc[int(id)] = voc
+        target_lookup[voc]=int(id)
     
     tran_path = dir_path + "/output.t1.5.bin"
-    tranLookup = {}
+    tran_lookup = {}
     with open(tran_path, "rb") as file:
         byte = file.read(4)
         while byte:
-            sourceID = int.from_bytes(byte,"big")
-            assert(sourceID == 0 or sourceID in sourceVoc.keys())
+            source_id = int.from_bytes(byte,"big")
+            assert(source_id == 0 or source_id in source_voc.keys())
             byte = file.read(4)
-            targetID = int.from_bytes(byte,"big")
-            assert(targetID in targetVoc.keys())
+            target_id = int.from_bytes(byte,"big")
+            assert(target_id in target_voc.keys())
             byte = file.read(4)
-            tranProb = intBitsToFloat(int.from_bytes(byte,"big"))
-            if (targetID in tranLookup.keys()) and (tranProb>minProb):
-                tranLookup[targetID][sourceID] = tranProb
-            elif tranProb>minProb:
-                tranLookup[targetID] = {}
-                tranLookup[targetID][sourceID] = tranProb
+            tran_prob = intbits_to_float(int.from_bytes(byte,"big"))
+            if (target_id in tran_lookup.keys()) and (tran_prob>MIN_PROB):
+                tran_lookup[target_id][source_id] = tran_prob
+            elif tran_prob>MIN_PROB:
+                tran_lookup[target_id] = {}
+                tran_lookup[target_id][source_id] = tran_prob
             byte = file.read(4)
-    return rescale(sourceLookup,targetLookup,tranLookup,targetVoc,sourceVoc)
+    return rescale(source_lookup,target_lookup,tran_lookup,target_voc,source_voc)
 
 
 def rank(qrels: str, base: str,tran_path:str, query_path:str, lucene_index_path: str,output_path:str, \
-        score_path:str,fieldName:str, tag: str,alpha:int,num_threads:int):
+        score_path:str,field_name:str, tag: str,alpha:int,num_threads:int):
 
     pool = ThreadPool(num_threads)
     searcher = JSimpleSearcher(JString(lucene_index_path))
     reader = JIndexReader().getReader(JString(lucene_index_path))
 
-    sourceLookup,targetLookup,tran = load_tranProbsTable(tran_path)
-    totalTermFreq = reader.getSumTotalTermFreq(fieldName)
+    source_lookup,target_lookup,tran = load_tranprobs_table(tran_path)
+    total_term_freq = reader.getSumTotalTermFreq(field_name)
     doc_dic = get_docs_from_qrun_by_topic(base)
     
     f = open(output_path, 'w')
@@ -252,13 +251,13 @@ def rank(qrels: str, base: str,tran_path:str, query_path:str, lucene_index_path:
         if i % 100==0:
             print(f"Reranking {i} query")
         i=i+1
-        query_text_lst = query[topic][fieldName]
-        collectProbs ={}
+        query_text_lst = query[topic][field_name]
+        collect_probs ={}
         for querytoken in query_text_lst:
-            collectProbs[querytoken] = max(reader.totalTermFreq(JTerm(fieldName, querytoken))/totalTermFreq, minCollectProb)
+            collect_probs[querytoken] = max(reader.totalTermFreq(JTerm(field_name, querytoken))/total_term_freq, MIN_COLLECT_PROB)
         arguments = [{"query_text_lst":query_text_lst,"test_doc":test_doc, "searcher":searcher,\
-                "fieldName":fieldName,"sourceLookup":sourceLookup,"targetLookup":targetLookup,\
-                "tran":tran,"collectProbs":collectProbs} for test_doc in test_docs]
+                "field_name":field_name,"source_lookup":source_lookup,"target_lookup":target_lookup,\
+                "tran":tran,"collect_probs":collect_probs} for test_doc in test_docs]
         rank_scores = pool.map(get_ibm_score, arguments)   
 
         ibm_scores = normalize([p for p in rank_scores])
@@ -287,7 +286,7 @@ if __name__ == '__main__':
                         metavar="path_to_qrels", help='path to new_qrels file')
     parser.add_argument('-base', type=str, default="../ibm/run.msmarco-passage.bm25tuned.trec",
                         metavar="path_to_base_run", help='path to base run')
-    parser.add_argument('-tran_path', type=str, default="../ibm/ibm_model/text_bert_tok_raw",
+    parser.add_argument('-tran_path', type=str, default="../ibm/ibm_model/text_bert_tok",
                         metavar="directory_path", help='directory path to source.vcb target.vcb and Transtable bin file')
     parser.add_argument('-query_path', type=str, default="../ibm/queries.dev.small.json",
                         metavar="path_to_query", help='path to dev queries file')
@@ -297,7 +296,7 @@ if __name__ == '__main__':
                         metavar="path_to_reranked_run", help='the path to store reranked run file')
     parser.add_argument('-score_path', type=str, default="../ibm/result-ibm-0.1.json",
                         metavar="path_to_base_run", help='the path to map and ndcg scores')
-    parser.add_argument('-fieldName', type=str, default="text_bert_tok",
+    parser.add_argument('-field_name', type=str, default="text_bert_tok",
                         metavar="type of field", help='type of field used for training')
     parser.add_argument('-alpha', type=float, default="0.1",
                         metavar="type of field", help='interpolation weight')
@@ -308,4 +307,4 @@ if __name__ == '__main__':
     print('Using base run:', args.base)
 
     rank(args.qrels, args.base, args.tran_path, args.query_path, args.index, args.output, \
-        args.score_path,args.fieldName, args.tag,args.alpha,args.num_threads)
+        args.score_path,args.field_name, args.tag,args.alpha,args.num_threads)
