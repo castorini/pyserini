ef163cd82af3439f1c9505765f05a79b75ea6b26
Yuqi Liu <y899liu@uwaterloo.ca>
2022-01-19 18:18:54 +0800

    remove unecessary sys.path update

--- scripts/reranker_ibm_colbert.py
+++ scripts/reranker_ibm_colbert.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import sys
-sys.path.append('.')
 import argparse
 import json
 import math
