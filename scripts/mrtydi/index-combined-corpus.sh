3af3c4d59201f67a58e2547b0b7c099c4c16a557
Xinyu Crystina ZHANG <x978zhan@uwaterloo.ca>
2022-02-23 16:31:43 -0500

    add download before index

--- scripts/mrtydi/index-combined-corpus.sh
+++ scripts/mrtydi/index-combined-corpus.sh
@@ -1,6 +1,20 @@
-collection_dir=$1  # the dir to the combined collection 
-index_dir=all-language-optimize
+set -e
 
+collection_dir=all-mbert-mrtydi-corpus  # the dir to the combined collection 
+index_dir=all-language-index-optimized
+
+mkdir -p $collection_dir
+
+# download files
+for lang in arabic  bengali  english finnish  indonesian  japanese  korean  russian  swahili  telugu  thai ; do
+	echo "Downloading $lang corpus"
+	lang_dir=$collection_dir/$lang
+	mkdir -p $lang_dir
+	wget "https://huggingface.co/datasets/crystina-z/mbert-mrtydi-corpus/resolve/main/mr-tydi-v1.1-mbert-tokenize-$lang/corpus.jsonl.gz" -P $lang_dir
+done
+
+
+# index
 python -m pyserini.index  \
     -collection MrTyDiCollection \
     -generator DefaultLuceneDocumentGenerator \
