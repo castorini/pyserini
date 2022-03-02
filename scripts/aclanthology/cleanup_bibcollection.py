5ddd61d464f9e81722fa8b2f9f0bbb51daf2b957
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2022-03-02 14:59:05 -0500

    acl anthology formatting

---
+++ scripts/aclanthology/cleanup_bibcollection.py
@@ -0,0 +1,72 @@
+import os
+from tqdm import tqdm
+import argparse
+import bibtexparser
+from bibtexparser.bibdatabase import BibDatabase
+from bibtexparser.bwriter import BibTexWriter
+
+"""
+pip install bibtexparser==1.2.0
+"""
+
+
+def format(bibtex):
+    bibtext = bibtex.split(",\n")
+    all_cat = []
+    for category in bibtext:
+        if "=" in category:
+            category = category.split(" = ")
+            end = " ".join(category[1:])
+            if not (end[-1] == '"' and end[0] == '"'):
+                category[-1] = category[-1].replace('"', "")
+                category[-1] = '"' + category[-1] + '"'
+            category = " = ".join(category)
+        all_cat.append(category)
+
+    return ",\n".join(all_cat)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Bibtex Parser")
+    parser.add_argument(
+        "--input_path", type=str, required=True, help="path to input bib collection"
+    )
+    parser.add_argument(
+        "--output_path", type=str, required=True, help="path to output collection"
+    )
+    args = parser.parse_args()
+
+    with open(args.input_path) as f:
+        text = []
+        for line in f:
+            if not line.isspace():
+                text.append(line)
+        text = "".join(text)
+        text = text.split("\n}\n")
+        text = [bib + "\n}" for bib in text[:-1]]
+
+    bib_collection = []
+    for item in tqdm(text, desc="parsing bib collection"):
+        try:
+            item = bibtexparser.loads(item)
+            bib_collection.append(item.entries[0])
+        except:
+            try:
+                item = format(item)
+                item = bibtexparser.loads(item)
+                bib_collection.append(item.entries[0])
+            except Exception as e:
+                print("Unable to Parse : \n" + item)
+
+    db = BibDatabase()
+    db.entries = bib_collection
+    writer = BibTexWriter()
+    writer.indent = "    "
+    writer.comma_first = False
+
+    with open(args.output_path, "w") as bibtex_file:
+        bibtex_file.write(writer.write(db))
+
+
+if __name__ == "__main__":
+    main()
