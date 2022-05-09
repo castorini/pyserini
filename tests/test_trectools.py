1c05b69d06bdd48224412c18064779c2062db216
Jimmy Lin <jimmylin@uwaterloo.ca>
2022-05-09 17:32:54 -0400

    Fix broken tests related to trec_eval computing unjudged (#1159)

--- tests/test_trectools.py
+++ tests/test_trectools.py
@@ -78,24 +78,25 @@ class TestTrecTools(unittest.TestCase):
         self.assertTrue(filecmp.cmp(os.path.join(self.root, 'tests/resources/simple_trec_run_normalize_verify.txt'),
                                     self.output_path))
 
-    def test_judge_score(self):
+    # This and the next test case go together - to keep and to remove unjudged docs.
+    def test_undjudged_keep(self):
         qrels_path = os.path.join(self.root, 'tools/topics-and-qrels/qrels.covid-round1.txt')
         run_path = os.path.join(self.root, 'tests/resources/simple_trec_run_filter.txt')
-        results = subprocess.check_output(f"python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 {qrels_path} {run_path}",shell=True)
-        results = '\n'.join(results.decode("utf-8").split('\n')[-7:])
-        with open(self.output_path,'w') as writer:
+        results = subprocess.check_output(f"python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 {qrels_path} {run_path}", shell=True)
+        results = '\n'.join(results.decode("utf-8").split('\n')[-5:])
+        with open(self.output_path, 'w') as writer:
             writer.write(results)
-        self.assertTrue(filecmp.cmp(os.path.join(self.root, 'tests/resources/simple_trec_run_get_judged.txt'),
+        self.assertTrue(filecmp.cmp(os.path.join(self.root, 'tests/resources/simple_trec_run_unjudged_keep.txt'),
                                     self.output_path))
 
     def test_remove_undjudged(self):
         qrels_path = os.path.join(self.root, 'tools/topics-and-qrels/qrels.covid-round1.txt')
         run_path = os.path.join(self.root, 'tests/resources/simple_trec_run_filter.txt')
-        results = subprocess.check_output(f"python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 {qrels_path} {run_path} -remove-unjudged",shell=True)
-        results = '\n'.join(results.decode("utf-8").split('\n')[-7:])
-        with open(self.output_path,'w') as writer:
+        results = subprocess.check_output(f"python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 {qrels_path} {run_path} -remove-unjudged", shell=True)
+        results = '\n'.join(results.decode("utf-8").split('\n')[-5:])
+        with open(self.output_path, 'w') as writer:
             writer.write(results)
-        self.assertTrue(filecmp.cmp(os.path.join(self.root, 'tests/resources/simple_trec_run_remove_unjudged.txt'),
+        self.assertTrue(filecmp.cmp(os.path.join(self.root, 'tests/resources/simple_trec_run_unjudged_remove.txt'),
                                     self.output_path))
 
     def tearDown(self):
