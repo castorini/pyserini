f019d3a0ffa6ae4dfdc7e02260e80e9fe2fda25d
lintool <jimmylin@uwaterloo.ca>
2022-05-14 17:26:03 -0400

    Refactor integration tests to reduce code duplication.

--- integrations/sparse/test_prebuilt_msmarco_v1_passage.py
+++ integrations/sparse/test_prebuilt_msmarco_v1_passage.py
@@ -16,10 +16,9 @@
 
 """Integration tests for MS MARCO V1 passage corpus using pre-built indexes."""
 
-import os
 import unittest
 
-from integrations.utils import clean_files, run_command, parse_score, parse_score_msmarco_as_string
+from integrations.utils import run_retrieval_and_return_scores, clean_files
 
 
 class TestPrebuiltMsMarcoV1Passage(unittest.TestCase):
@@ -27,120 +26,64 @@ class TestPrebuiltMsMarcoV1Passage(unittest.TestCase):
         self.temp_files = []
 
     def test_passage_trec_output(self):
-        output_file = 'runs/test_run.msmarco-passage.1.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage --output {output_file} --bm25'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
+        """Test case for MS MARCO V1 passage, dev queries, TREC output
+           on all three pre-built indexes (base, slim, full)."""
 
-        cmd = f'python -m pyserini.eval.trec_eval -c -m map msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'map')
-        self.assertAlmostEqual(score, 0.1958, delta=0.0001)
+        # Loop over all three pre-built indexes.
+        for index in ['msmarco-v1-passage', 'msmarco-v1-passage-slim', 'msmarco-v1-passage-full']:
+            scores = run_retrieval_and_return_scores(
+                'runs/test_run.msmarco-passage.trec.txt',
+                f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index {index} --bm25',
+                'msmarco-passage-dev-subset',
+                'trec_eval',
+                [['map', 'map'], ['recall.1000', 'recall_1000']])
 
-        cmd = f'python -m pyserini.eval.trec_eval -c -m recall.1000 msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'recall_1000')
-        self.assertAlmostEqual(score, 0.8573, delta=0.0001)
+            self.assertTrue('map' in scores)
+            self.assertTrue('recall.1000' in scores)
+            self.assertAlmostEqual(scores['map'], 0.1958, delta=0.0001)
+            self.assertAlmostEqual(scores['recall.1000'], 0.8573, delta=0.0001)
 
     def test_passage_msmarco_output(self):
-        output_file = 'runs/test_run.msmarco-passage.2.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage --output {output_file} --bm25 --output-format msmarco'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
+        """Test case for MS MARCO V1 passage, dev queries, MS MARCO output
+           on all three pre-built indexes (base, slim, full)."""
 
-        cmd = f'python -m pyserini.eval.msmarco_passage_eval msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score_msmarco_as_string(stdout, 'MRR @10')
-        self.assertEqual(score, '0.18741227770955546')
+        # Loop over all three pre-built indexes.
+        for index in ['msmarco-v1-passage', 'msmarco-v1-passage-slim', 'msmarco-v1-passage-full']:
+            scores = run_retrieval_and_return_scores(
+                'runs/test_run.msmarco-passage.msmarco.txt',
+                f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index {index} --bm25 --output-format msmarco',
+                'msmarco-passage-dev-subset',
+                'msmarco_passage_string', [])
 
-    def test_passage_slim_trec_output(self):
-        output_file = 'runs/test_run.msmarco-passage.3.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-slim --output {output_file} --bm25'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
-
-        cmd = f'python -m pyserini.eval.trec_eval -c -m map msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'map')
-        self.assertAlmostEqual(score, 0.1958, delta=0.0001)
-
-        cmd = f'python -m pyserini.eval.trec_eval -c -m recall.1000 msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'recall_1000')
-        self.assertAlmostEqual(score, 0.8573, delta=0.0001)
-
-    def test_passage_slim_msmarco_output(self):
-        output_file = 'runs/test_run.msmarco-passage.4.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-slim --output {output_file} --bm25 --output-format msmarco'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
-
-        cmd = f'python -m pyserini.eval.msmarco_passage_eval msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score_msmarco_as_string(stdout, 'MRR @10')
-        self.assertEqual(score, '0.18741227770955546')
-
-    def test_passage_full_trec_output(self):
-        output_file = 'runs/test_run.msmarco-passage.5.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-full --output {output_file} --bm25'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
-
-        cmd = f'python -m pyserini.eval.trec_eval -c -m map msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'map')
-        self.assertAlmostEqual(score, 0.1958, delta=0.0001)
-
-        cmd = f'python -m pyserini.eval.trec_eval -c -m recall.1000 msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'recall_1000')
-        self.assertAlmostEqual(score, 0.8573, delta=0.0001)
-
-    def test_passage_full_msmarco_output(self):
-        output_file = 'runs/test_run.msmarco-passage.6.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-full --output {output_file} --bm25 --output-format msmarco'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
-
-        cmd = f'python -m pyserini.eval.msmarco_passage_eval msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score_msmarco_as_string(stdout, 'MRR @10')
-        self.assertEqual(score, '0.18741227770955546')
+            self.assertTrue('MRR@10' in scores)
+            self.assertAlmostEqual(scores['MRR@10'], '0.18741227770955546')
 
     def test_passage_expanded_trec_output(self):
-        output_file = 'runs/test_run.msmarco-passage.expanded.1.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-d2q-t5 --output {output_file} --bm25'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
+        """Test case for MS MARCO V1 passage w/ doc2query-T5 expansions, dev queries, TREC output."""
 
-        cmd = f'python -m pyserini.eval.trec_eval -c -m map msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'map')
-        self.assertAlmostEqual(score, 0.2893, delta=0.0001)
+        scores = run_retrieval_and_return_scores(
+            'runs/test_run.msmarco-passage.expanded.trec.txt',
+            f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-d2q-t5 --bm25',
+            'msmarco-passage-dev-subset',
+            'trec_eval',
+            [['map', 'map'], ['recall.1000', 'recall_1000']])
 
-        cmd = f'python -m pyserini.eval.trec_eval -c -m recall.1000 msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score(stdout, 'recall_1000')
-        self.assertAlmostEqual(score, 0.9506, delta=0.0001)
+        self.assertTrue('map' in scores)
+        self.assertTrue('recall.1000' in scores)
+        self.assertAlmostEqual(scores['map'], 0.2893, delta=0.0001)
+        self.assertAlmostEqual(scores['recall.1000'], 0.9506, delta=0.0001)
 
     def test_passage_expanded_msmarco_output(self):
-        output_file = 'runs/test_run.msmarco-passage.expanded.2.txt'
-        self.temp_files.append(output_file)
-        cmd = f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-d2q-t5 --output {output_file} --bm25 --output-format msmarco'
-        status = os.system(cmd)
-        self.assertEqual(status, 0)
+        """Test case for MS MARCO V1 passage w/ doc2query-T5 expansions, dev queries, MS MARCO output."""
 
-        cmd = f'python -m pyserini.eval.msmarco_passage_eval msmarco-passage-dev-subset {output_file}'
-        stdout, stderr = run_command(cmd)
-        score = parse_score_msmarco_as_string(stdout, 'MRR @10')
-        self.assertEqual(score, '0.281560751807885')
+        scores = run_retrieval_and_return_scores(
+            'runs/test_run.msmarco-passage.expanded.msmarco.txt',
+            f'python -m pyserini.search.lucene --topics msmarco-passage-dev-subset --index msmarco-v1-passage-d2q-t5 --bm25 --output-format msmarco',
+            'msmarco-passage-dev-subset',
+            'msmarco_passage_string', [])
+
+        self.assertTrue('MRR@10' in scores)
+        self.assertAlmostEqual(scores['MRR@10'], '0.281560751807885')
 
     def tearDown(self):
         clean_files(self.temp_files)
