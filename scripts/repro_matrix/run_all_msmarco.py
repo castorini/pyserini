89be505cb029be17f7a5877d2ca5522600413bb3
lintool <jimmylin@uwaterloo.ca>
2022-05-28 07:21:35 -0400

    HTML rendering, spacing tweaks.

--- scripts/repro_matrix/run_all_msmarco.py
+++ scripts/repro_matrix/run_all_msmarco.py
@@ -100,26 +100,26 @@ if __name__ == '__main__':
                     print('')
 
     if collection == 'msmarco-v1-passage' or collection == 'msmarco-v1-doc':
-        print(' ' * 64 + 'TREC 2019' + ' ' * 16 + 'TREC 2020' + ' ' * 12 + 'MS MARCO dev')
-        print(' ' * 57 + 'MAP    nDCG@10    R@1K       MAP nDCG@10    R@1K    MRR@10    R@1K')
-        print(' ' * 57 + '-' * 22 + '    ' + '-' * 22 + '    ' + '-' * 14)
+        print(' ' * 69 + 'TREC 2019' + ' ' * 16 + 'TREC 2020' + ' ' * 12 + 'MS MARCO dev')
+        print(' ' * 62 + 'MAP    nDCG@10    R@1K       MAP nDCG@10    R@1K    MRR@10    R@1K')
+        print(' ' * 62 + '-' * 22 + '    ' + '-' * 22 + '    ' + '-' * 14)
         for name in models[collection]:
             if not name:
                 print('')
                 continue
-            print(f'{table_keys[name]:55}' +
+            print(f'{table_keys[name]:60}' +
                   f'{table[name]["dl19"]["MAP"]:8.4f}{table[name]["dl19"]["nDCG@10"]:8.4f}{table[name]["dl19"]["R@1K"]:8.4f}  ' +
                   f'{table[name]["dl20"]["MAP"]:8.4f}{table[name]["dl20"]["nDCG@10"]:8.4f}{table[name]["dl20"]["R@1K"]:8.4f}  ' +
                   f'{table[name]["dev"]["MRR@10"]:8.4f}{table[name]["dev"]["R@1K"]:8.4f}')
     else:
-        print(' ' * 72 + 'TREC 2021' + ' ' * 18 + 'MS MARCO dev' + ' ' * 6 + 'MS MARCO dev2')
-        print(' ' * 57 + 'MAP@100 nDCG@10 MRR@100 R@100   R@1K     MRR@100   R@1K    MRR@100   R@1K')
-        print(' ' * 57 + '-' * 38 + '    ' + '-' * 14 + '    ' + '-' * 14)
+        print(' ' * 77 + 'TREC 2021' + ' ' * 18 + 'MS MARCO dev' + ' ' * 6 + 'MS MARCO dev2')
+        print(' ' * 62 + 'MAP@100 nDCG@10 MRR@100 R@100   R@1K     MRR@100   R@1K    MRR@100   R@1K')
+        print(' ' * 62 + '-' * 38 + '    ' + '-' * 14 + '    ' + '-' * 14)
         for name in models[collection]:
             if not name:
                 print('')
                 continue
-            print(f'{table_keys[name]:55}' +
+            print(f'{table_keys[name]:60}' +
                   f'{table[name]["dl21"]["MAP@100"]:8.4f}{table[name]["dl21"]["nDCG@10"]:8.4f}' +
                   f'{table[name]["dl21"]["MRR@100"]:8.4f}{table[name]["dl21"]["R@100"]:8.4f}{table[name]["dl21"]["R@1K"]:8.4f}  ' +
                   f'{table[name]["dev"]["MRR@100"]:8.4f}{table[name]["dev"]["R@1K"]:8.4f}  ' +
