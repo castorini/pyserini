e641d4dfe3e3bbf8416ce0f9cf2c0d5be777f3e5
Jimmy Lin <jimmylin@uwaterloo.ca>
2022-05-31 12:15:46 -0400

    Change trec_eval output spacing back to previous alignment (#1193)
    Ref #1184

--- tests/test_trectools.py
+++ tests/test_trectools.py
@@ -83,8 +83,8 @@ class TestTrecTools(unittest.TestCase):
         qrels_path = os.path.join(self.root, 'tools/topics-and-qrels/qrels.covid-round1.txt')
         run_path = os.path.join(self.root, 'tests/resources/simple_trec_run_filter.txt')
         results = subprocess.check_output(
-            f'python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 {qrels_path} {run_path}', shell=True)
-        results = '\n'.join(results.decode('utf-8').split('\n')[-4:])
+            f'python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 -m judged.5,10 {qrels_path} {run_path}', shell=True)
+        results = '\n'.join(results.decode('utf-8').split('\n')[-6:])
         with open(self.output_path, 'w') as writer:
             writer.write(results)
         self.assertTrue(filecmp.cmp(os.path.join(self.root, 'tests/resources/simple_trec_run_unjudged_keep.txt'),
@@ -94,8 +94,8 @@ class TestTrecTools(unittest.TestCase):
         qrels_path = os.path.join(self.root, 'tools/topics-and-qrels/qrels.covid-round1.txt')
         run_path = os.path.join(self.root, 'tests/resources/simple_trec_run_filter.txt')
         results = subprocess.check_output(
-            f'python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 {qrels_path} {run_path} -remove-unjudged', shell=True)
-        results = '\n'.join(results.decode('utf-8').split('\n')[-4:])
+            f'python -m pyserini.eval.trec_eval -m ndcg_cut.5,10 -m judged.5,10 {qrels_path} {run_path} -remove-unjudged', shell=True)
+        results = '\n'.join(results.decode('utf-8').split('\n')[-6:])
         with open(self.output_path, 'w') as writer:
             writer.write(results)
         self.assertTrue(filecmp.cmp(os.path.join(self.root, 'tests/resources/simple_trec_run_unjudged_remove.txt'),
