e982226fe9b27f841f29299c8aa81d757a82f8a7
Alexei I <a7ivanov@uwaterloo.ca>
2022-08-25 14:38:31 -0400

    ..

--- scripts/trec-ct/sanity_check.py
+++ scripts/trec-ct/sanity_check.py
@@ -22,22 +22,24 @@ def get_top_n_res_per_topic(run: pd.DataFrame, queries:pd.DataFrame, collection:
         topic_str = queries[queries["TOPIC_NO"] == topic_id]["QUERY"].iloc[0]
         res_str = collection[collection["id"] == res_id]["contents"].iloc[0]
 
-        if topic_str in query_str_to_top_n_result_str:
-            query_str_to_top_n_result_str[topic_str].append(res_str)
+        # {TOPIC_ID: [query, res_1, res_2, ..., res_n]}
+        if topic_id in query_str_to_top_n_result_str:
+            query_str_to_top_n_result_str[topic_id].append(res_str)
         else:
-            query_str_to_top_n_result_str[topic_str] = [res_str]
+            query_str_to_top_n_result_str[topic_id] = [topic_str, res_str]
     
     return query_str_to_top_n_result_str
 
 def print_top_n_res(topics_to_res: Dict):
-    for topic, res in topics_to_res.items():
-        print(topic)
+    for topic_id, res in topics_to_res.items():
+        print(f"QUERY_ID: {topic_id} \t QUERY_TEXT: {res[0]}")
         print()
         wrapper = textwrap.TextWrapper(initial_indent=' '*10, width=120, subsequent_indent=' '*10)
-        for item in res:
+        for rank, item in enumerate(res[1:]):
+            print(wrapper.fill(f"Result #{rank+1}:"))
             print(wrapper.fill(item))
             print()
-    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--collection", required=True, type=str, help='collection json file')
@@ -52,7 +54,7 @@ if __name__ == '__main__':
         for line in f:
             data.append(json.loads(line))
         
-    collection: pd.DataFrame = pd.DataFrame(data)
+    collection = pd.DataFrame(data)
     run: pd.DataFrame = pd.read_csv(args.run, sep=" ", names=TREC_SUBMISSION_FILE_COL_NAMES)
     queries: pd.DataFrame = pd.read_csv(args.queries, sep="\t", names=["TOPIC_NO", "QUERY"])
 
