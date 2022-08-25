c57d7bc59d01d68c46cdd38d61903fa32d3d9d95
Alexei I <a7ivanov@uwaterloo.ca>
2022-08-25 14:19:39 -0400

    ..

--- scripts/trec-ct/sanity_check.py
+++ scripts/trec-ct/sanity_check.py
@@ -8,7 +8,6 @@ TREC_SUBMISSION_FILE_COL_NAMES = ["TOPIC_NO", "Q0", "ID", "RANK", "SCORE", "RUN_
 
 
 def get_top_n_res_per_topic(run: pd.DataFrame, queries:pd.DataFrame, collection: pd.DataFrame, n: int):
-    ## assumed the run is already sorted
     n_ranks = [i for i in range(1, n+1)]
     top_n_res_per_query = run[run["RANK"].isin(n_ranks)]
     top_n_res_per_query = top_n_res_per_query.sort_values("RANK")
@@ -54,7 +53,7 @@ if __name__ == '__main__':
         for line in f:
             data.append(json.loads(line))
         
-    collection = pd.DataFrame(data)
+    collection: pd.DataFrame = pd.DataFrame(data)
     run: pd.DataFrame = pd.read_csv(args.run, sep=" ", names=TREC_SUBMISSION_FILE_COL_NAMES)
     queries: pd.DataFrame = pd.read_csv(args.queries, sep="\t", names=["TOPIC_NO", "QUERY"])
 
