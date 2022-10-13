a70dd53fdcc20425f28d5e644cb7e9fe171f4afb
Crystina <x978zhan@uwaterloo.ca>
2022-10-13 15:44:32 -0400

    MIRACL index (bm25) (#1296)
    bm25 index on MIRACL data (url, stats, doc)

--- pyserini/prebuilt_index_info.py
+++ pyserini/prebuilt_index_info.py
@@ -4018,4 +4018,228 @@ FAISS_INDEX_INFO = {
         "downloaded": False,
         "texts": "mrtydi-v1.1-thai"
     },
+    "miracl-v1.0-ar": {
+        "description": "Lucene index for MIRACL v1.0 (Arabic).",
+        "filename": "lucene-index.miracl-v1.0-ar.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-ar.20221004.2b2856.tar.gz"
+        ],
+        "md5": "503d3b49a557222d8074ac831a2f047a",
+        "size compressed (bytes)": 1193292491,
+        "total_terms": 90223450,
+        "documents": 2061414,
+        "unique_terms": 1246254,
+        "downloaded": False
+    },
+    "miracl-v1.0-bn": {
+        "description": "Lucene index for MIRACL v1.0 (Bengali).",
+        "filename": "lucene-index.miracl-v1.0-bn.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-bn.20221004.2b2856.tar.gz"
+        ],
+        "md5": "7a20210328f0b83f44e041f0c94d30e2",
+        "size compressed (bytes)": 236113202,
+        "total_terms": 14963235,
+        "documents": 297265,
+        "unique_terms": 506812,
+        "downloaded": False
+    },
+    "miracl-v1.0-en": {
+        "description": "Lucene index for MIRACL v1.0 (English).",
+        "filename": "lucene-index.miracl-v1.0-en.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-en.20221004.2b2856.tar.gz"
+        ],
+        "md5": "4fbd652deb76bcc05daa35392d4aa9f3",
+        "size compressed (bytes)": 17823436054,
+        "total_terms": 1505029955,
+        "documents": 32893221,
+        "unique_terms": 6152316,
+        "downloaded": False
+    },
+    "miracl-v1.0-es": {
+        "description": "Lucene index for MIRACL v1.0 (Spanish).",
+        "filename": "lucene-index.miracl-v1.0-es.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-es.20221004.2b2856.tar.gz"
+        ],
+        "md5": "b4c9993ee3a131871d4f07dd96e80531",
+        "size compressed (bytes)": 5474245249,
+        "total_terms": 389319806,
+        "documents": 10373953,
+        "unique_terms": 2907509,
+        "downloaded": False
+    },
+    "miracl-v1.0-fa": {
+        "description": "Lucene index for MIRACL v1.0 (Persian).",
+        "filename": "lucene-index.miracl-v1.0-fa.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-fa.20221004.2b2856.tar.gz"
+        ],
+        "md5": "bfc824aa37633e3d45bcfd5c5e0e1701",
+        "size compressed (bytes)": 1023090577,
+        "total_terms": 67968038,
+        "documents": 2207172,
+        "unique_terms": 1208930,
+        "downloaded": False
+    },
+    "miracl-v1.0-fi": {
+        "description": "Lucene index for MIRACL v1.0 (Finnish).",
+        "filename": "lucene-index.miracl-v1.0-fi.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-fi.20221004.2b2856.tar.gz"
+        ],
+        "md5": "4197c90efd781c6153acaf15452c5479",
+        "size compressed (bytes)": 925422988,
+        "total_terms": 68295087,
+        "documents": 1883509,
+        "unique_terms": 1669817,
+        "downloaded": False
+    },
+    "miracl-v1.0-fr": {
+        "description": "Lucene index for MIRACL v1.0 (French).",
+        "filename": "lucene-index.miracl-v1.0-fr.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-fr.20221004.2b2856.tar.gz"
+        ],
+        "md5": "e68b10d90be71b702888a3d00a8aa39c",
+        "size compressed (bytes)": 6747612709,
+        "total_terms": 508723988,
+        "documents": 14636953,
+        "unique_terms": 2811342,
+        "downloaded": False
+    },
+    "miracl-v1.0-hi": {
+        "description": "Lucene index for MIRACL v1.0 (Hindi).",
+        "filename": "lucene-index.miracl-v1.0-hi.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-hi.20221004.2b2856.tar.gz"
+        ],
+        "md5": "d81f4e2b7ec5df8f9741168c23c977e2",
+        "size compressed (bytes)": 340997734,
+        "total_terms": 21080143,
+        "documents": 506264,
+        "unique_terms": 597558,
+        "downloaded": False
+    },
+    "miracl-v1.0-id": {
+        "description": "Lucene index for MIRACL v1.0 (Indonesian).",
+        "filename": "lucene-index.miracl-v1.0-id.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-id.20221004.2b2856.tar.gz"
+        ],
+        "md5": "b1092e732991029fae7c542e5e129255",
+        "size compressed (bytes)": 577263718,
+        "total_terms": 51469219,
+        "documents": 1446315,
+        "unique_terms": 911944,
+        "downloaded": False
+    },
+    "miracl-v1.0-ja": {
+        "description": "Lucene index for MIRACL v1.0 (Japanese).",
+        "filename": "lucene-index.miracl-v1.0-ja.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-ja.20221004.2b2856.tar.gz"
+        ],
+        "md5": "4db9550d0af63736a0fd2b486b3b7273",
+        "size compressed (bytes)": 3745158372,
+        "total_terms": 296659169,
+        "documents": 6953614,
+        "unique_terms": 1558643,
+        "downloaded": False
+    },
+    "miracl-v1.0-ko": {
+        "description": "Lucene index for MIRACL v1.0 (Korean).",
+        "filename": "lucene-index.miracl-v1.0-ko.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-ko.20221004.2b2856.tar.gz"
+        ],
+        "md5": "c82f5c7641fd78b8dadfcb279a1c0340",
+        "size compressed (bytes)": 1150899287,
+        "total_terms": 121464424,
+        "documents": 1486752,
+        "unique_terms": 1504782,
+        "downloaded": False
+    },
+    "miracl-v1.0-ru": {
+        "description": "Lucene index for MIRACL v1.0 (Russian).",
+        "filename": "lucene-index.miracl-v1.0-ru.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-ru.20221004.2b2856.tar.gz"
+        ],
+        "md5": "c1b974e298d9e1deeccae8b84a5bcd64",
+        "size compressed (bytes)": 6003987738,
+        "total_terms": 343106870,
+        "documents": 9543918,
+        "unique_terms": 2955627,
+        "downloaded": False
+    },
+    "miracl-v1.0-sw": {
+        "description": "Lucene index for MIRACL v1.0 (Swahili).",
+        "filename": "lucene-index.miracl-v1.0-sw.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-sw.20221004.2b2856.tar.gz"
+        ],
+        "md5": "64b77bcc11e04575d0723ad81ac7c135",
+        "size compressed (bytes)": 45410264,
+        "total_terms": 4752278,
+        "documents": 131924,
+        "unique_terms": 361306,
+        "downloaded": False
+    },
+    "miracl-v1.0-te": {
+        "description": "Lucene index for MIRACL v1.0 (Telugu).",
+        "filename": "lucene-index.miracl-v1.0-te.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-te.20221004.2b2856.tar.gz"
+        ],
+        "md5": "1f78c68678f439a3143a6fb0d25bfe27",
+        "size compressed (bytes)": 402045711,
+        "total_terms": 26105595,
+        "documents": 518079,
+        "unique_terms": 1120047,
+        "downloaded": False
+    },
+    "miracl-v1.0-th": {
+        "description": "Lucene index for MIRACL v1.0 (Thai).",
+        "filename": "lucene-index.miracl-v1.0-th.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-th.20221004.2b2856.tar.gz"
+        ],
+        "md5": "eeef93c23b76fdc66b9e1ee01576765e",
+        "size compressed (bytes)": 431498349,
+        "total_terms": 29922100,
+        "documents": 542166,
+        "unique_terms": 626084,
+        "downloaded": False
+    },
+    "miracl-v1.0-zh": {
+        "description": "Lucene index for MIRACL v1.0 (Chinese).",
+        "filename": "lucene-index.miracl-v1.0-zh.20221004.2b2856.tar.gz",
+        "readme": "lucene-index.miracl-v1.0.20221004.2b2856.README.md",
+        "urls": [
+            "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.miracl-v1.0-zh.20221004.2b2856.tar.gz"
+        ],
+        "md5": "dc7880da333b7c56d3a4ff0bf018febd",
+        "size compressed (bytes)": 4212198217,
+        "total_terms": 423635495,
+        "documents": 4934368,
+        "unique_terms": 6517412,
+        "downloaded": False
+    },
 }
