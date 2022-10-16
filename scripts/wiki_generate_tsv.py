12f4b0afa037f856cfab0a87b085905d6bd94356
manveertamber <manveertamber@gmail.com>
2022-10-16 13:50:26 -0400

    Clean up wiki pre-processing scripts

--- scripts/wiki_generate_tsv.py
+++ scripts/wiki_generate_tsv.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 import argparse
 from tqdm import tqdm
 import sqlite3
@@ -26,17 +25,20 @@ from pygaggle.rerank.base import Text
 from pygaggle.data.segmentation import SegmentProcessor
 import pickle
 
+'''After WikiExtractor and https://github.com/facebookresearch/DrQA/tree/main/scripts/retriever pre-processing is done on the XML dump, 
+the final pre-processing is done in this script to generate the WIKI-TL_6_3 and WIKI-TL_8_4 corpuses (with tables and lists).
+Note: This requires a lot of memory because the entire XML dump is loaded into memory. '''
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='generate .tsv files for wiki corpuses with tables and lists')
+    parser = argparse.ArgumentParser(description='generate .tsv files for wiki corpuses of passages with tables and lists')
     parser.add_argument('-db_path', type=str, required=True, help='path to .db file containing preprocessed wiki pages from DrQA')
-    parser.add_argument('-xml_path', type=str, required=True, help='path to wikipedia xml dump file')
+    parser.add_argument('-xml_path', type=str, required=True, help='path to full wikipedia xml dump file')
     parser.add_argument('-output_path_6_3', type=str, default="../collections/wiki_6_3.tsv", help='path to write .tsv with segment 6, stride 3')
     parser.add_argument('-output_path_8_4', type=str, default="../collections/wiki_8_4.tsv", help='path to write .tsv with segment 8, stride 4')
     args = parser.parse_args()
 
     sqliteConnection = sqlite3.connect(args.db_path)
     cursor = sqliteConnection.cursor()
-
     sqlite_select_query = "SELECT id, text FROM documents"
     cursor.execute(sqlite_select_query)
     pages = cursor.fetchall()
@@ -53,6 +55,7 @@ if __name__ == '__main__':
 
     parsed_pages_tables = {}
     infoboxes_params = {}
+    # Get tables and infoboxes from XML dump. Store them in parsed_pages_tables and infoboxes_params respectively.
     for page in tqdm(xml_pages):
         p = wtp.parse(page)    
         title = unidecode.unidecode(re.search('<title>(.*?)</title>', page).group(1).strip())
@@ -71,22 +74,15 @@ if __name__ == '__main__':
                 else:
                     infoboxes_params[title].append(template.params)
 
-    try:
-        with open('parsed_pages_tables.pickle', 'wb') as handle:
-            pickle.dump(parsed_pages_tables, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        with open('infoboxes_params.pickle', 'wb') as handle:
-            pickle.dump(infoboxes_params, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    except:
-        print("OOPS")
-
     documents = {}
 
-    # for matching tables from xml dump to articles in .db file
+    # For matching tables from xml dump to articles in .db file
     key_error_fails = 0
     missing_table_fails = 0
     table_data_fails = 0
     extracted_tables = 0
         
+    # To avoid duplicate pages
     for row in tqdm(pages):
         text = row[1]
         title = row[0]
@@ -105,11 +101,27 @@ if __name__ == '__main__':
     id1 = 1
     id2 = 1
     for document in tqdm(documents):
-        text =  documents[document]
+        texts = []
+        text = documents[document]
+        text = text.strip()
 
+        if text.startswith("REDIRECT") or text.startswith("redirect"):
+            continue
+        if text.endswith(". References."):
+            text = text[:-len(" References.")].strip()
+        
         infobox_text = ""
-        if document in infoboxes_params:
-            for infobox_params in infoboxes_params[document]:
+        # Need to match document name from XML dump with document name from .db from WikiExtractor and DrQA pre-processing
+        matching_doc_name = unidecode.unidecode(document.strip())
+        if matching_doc_name not in infoboxes_params:
+            matching_doc_name = saxutils.escape(matching_doc_name)
+            if matching_doc_name not in infoboxes_params:
+                 matching_doc_name = matching_doc_name.replace("\"", "&quot;")
+                 if matching_doc_name not in infoboxes_params:
+                    print("FAILED TO MATCH DOC TITLE", unidecode.unidecode(saxutils.escape(document.strip())))
+                    key_error_fails += 1
+        if matching_doc_name in infoboxes_params:
+            for infobox_params in infoboxes_params[matching_doc_name]:
                 for param in infobox_params:
                     param_name = str(param.name).strip().replace("\n", " ").replace("_", " ")
                     # no useful information
@@ -125,22 +137,18 @@ if __name__ == '__main__':
                         param_text = param_text.replace('|df=yes', '').replace('|mf=yes', '')
                         param_text = param_text.replace("|", " | ")
                         infobox_text += param_name + ": " + param_text + ". "
-                        
-        text = infobox_text + text
+            # add infobox text to beginning of text    
+            text = infobox_text + text
 
-        texts = []
         table_num = -1
-        text = text.strip()
         table_found = True
-        if text.startswith("REDIRECT") or text.startswith("redirect"):
-            continue
-        if text.endswith(". References."):
-            text = text[:-len(". References.")].strip()
         p_tables = []
+
+        # Need to match document name from XML dump with document name from .db from WikiExtractor and DrQA pre-processing
         matching_doc_name = unidecode.unidecode(document.strip())
         if matching_doc_name in parsed_pages_tables:
             p_tables = parsed_pages_tables[matching_doc_name]
-        else: 
+        else:
             matching_doc_name = saxutils.escape(matching_doc_name)
             if matching_doc_name in parsed_pages_tables:
                 p_tables = parsed_pages_tables[matching_doc_name]
@@ -152,11 +160,15 @@ if __name__ == '__main__':
                     print("FAILED TO MATCH DOC TITLE", unidecode.unidecode(saxutils.escape(document.strip())))
                     key_error_fails += 1
                     table_found = False
-        while table_found and "TABLETOREPLACE" in text : # trying to put the tables back in the appropriate location in the text
+        
+        # TABLETOREPLACE was added in the WikiExtractor step, where tables were replaced with the string TABLETOREPLACE so that they were included in the processed text corpus as TABLETOREPLACE, instead of gotten rid of by default.
+        while table_found and ("TABLETOREPLACE" in text or len(p_tables) > table_num + 1): 
+            # Trying to put the tables back in the appropriate location in the text
+            # However, might have parsed more tables in some article than there are occurences of TABLETOREPLACE in the article for some reason. Add these extra linearized tables to end of text.
             table_num +=1
             table_text = ""
             if len(p_tables) < table_num+1:
-                # failed to get table from dump
+                # Failed to get table from dump
                 #print("FAILED TO GET TABLE FROM DUMP, NOT ENOUGH TABLES PARSED", saxutils.escape(document.strip()))
                 missing_table_fails+=1
                 break
@@ -166,60 +178,35 @@ if __name__ == '__main__':
                 table_data = p_tables[table_num].data(span=False)
                 if len(table_data[0]) <= 2 and len(table_data) > 1 and len(table_data[0]) + 2 < len(table_data[1]):
                     for elem in table_data[0]:
-                        table_text += elem + ". "
+                        if elem.strip() != "":
+                            table_text += elem + ", "
                     table_data = table_data[1:]
                 if len(table_data) == 1:
-                    for elem in table_data[0]:
-                        table_text += elem + ". "
-            except:
-                table_data_fails+=1
-                continue
-            
-            # linearize table parsed from wikitextparser in format: "column_name1: row_elem1. column_name2: row_elem2. ..." for each row
-            for row in range(1, len(table_data)):
-                for column in range(len(table_data[row])):
-                    if column < len(table_data[0]) and table_data[row][column] is not None and str(table_data[row][column]).strip() != "":
-                        if table_data[0][column] is not None:
-                            table_text += table_data[0][column] + ": " 
-                        table_text += table_data[row][column] + ' '
-                table_text += ". " 
-            
-            table_text = table_text.replace("''", "")
-            table_text = table_text.replace("|", " | ")
-            # add table in appropriate place in text
-            text = text.replace("TABLETOREPLACE", '. ' + table_text.replace("\n", " "), 1)
-            extracted_tables += 1
-        # might have parsed more tables in some article than there are occurences of TABLETOREPLACE in the article for some reason. Add these linearized tables to end of text.
-        while table_found and len(p_tables) > table_num + 1:
-            table_num +=1
-            table_text = ""
-            
-            try:
-                table_data = p_tables[table_num].data(span=False)
-                if len(table_data[0]) <= 2 and len(table_data) > 1 and len(table_data[0]) + 2 < len(table_data[1]):
                     for elem in table_data[0]:
                         if elem.strip() != "":
-                            table_text += elem + ". "
-                    table_data = table_data[1:]
-                if len(table_data) == 1:
-                    for elem in table_data[0]:
-                        table_text += elem + ". "
-            except:
+                            table_text += elem + ", "
+                table_text += ". "
+            except: # working with p_tables[table_num].data sometimes throws unexplained errors
                 table_data_fails+=1
                 continue
-
+            
+            # linearize table parsed from wikitextparser in format: "column_name1: row_elem1, column_name2: row_elem2, ..." for each row
             for row in range(1, len(table_data)):
                 for column in range(len(table_data[row])):
                     if column < len(table_data[0]) and table_data[row][column] is not None and str(table_data[row][column]).strip() != "":
                         if table_data[0][column] is not None:
                             table_text += table_data[0][column] + ": " 
-                        table_text += table_data[row][column] + ' '
-                table_text += ". " 
-            
+                        table_text += table_data[row][column] + ', '
+                table_text += ". "
             table_text = table_text.replace("''", "")
-            text += " " + table_text
+            table_text = table_text.replace("|", " | ")
+            # Try to add table in appropriate place in text.
+            if "TABLETOREPLACE" in text:
+                text = text.replace("TABLETOREPLACE", '. ' + table_text.replace("\n", " "), 1)
+            else:
+                text += " " + table_text
             extracted_tables += 1
-
+       
         text = re.sub('\{\{cite .*?\}\}', ' ', text) 
         text = text.replace(r"TABLETOREPLACE", " ")
         text = text.replace(r"'''", " ")
@@ -235,7 +222,7 @@ if __name__ == '__main__':
         text = text.replace("formatnum:", "")
         
         #text = re.sub('<poem.*?</poem>', ' ', text, flags=re.DOTALL) # might have useful information?
-        text = re.sub('<math.*?</math>', '', text, flags=re.DOTALL) # is in latex could maybe parse into readable text?
+        text = re.sub('<math.*?</math>', '', text, flags=re.DOTALL)
         text = re.sub('<chem.*?</chem>', '', text, flags=re.DOTALL) 
         text = re.sub('<score.*?</score>', '', text, flags=re.DOTALL) 
 
@@ -255,7 +242,6 @@ if __name__ == '__main__':
         text = re.sub('\| ?ul_?style= ?.*? ', ' ', text)
         text = re.sub('\| ?li_?style= ?.*? ', ' ', text)
         text = re.sub('\| ?border-style= ?.*? ', ' ', text)
-    
         text = re.sub('\|? ?style=\".*?\"', '', text) 
         text = re.sub('\|? ?rowspan=\".*?\"', '', text)
         text = re.sub('\|? ?colspan=\".*?\"', '', text)
@@ -266,20 +252,19 @@ if __name__ == '__main__':
         text = re.sub('\|? ?bgcolor=\".*?\"', '', text)
         text = re.sub('\|? ?bg=\#[a-z]+', '', text)
         text = re.sub('\|? ?width=\".*?\"', '', text)
-
         text = re.sub('\|? ?height=[0-9]+', '', text)
         text = re.sub('\|? ?width=[0-9]+', '', text)
         text = re.sub('\|? ?rowspan=[0-9]+', '', text)
         text = re.sub('\|? ?colspan=[0-9]+', '', text)
-
         text = re.sub(r'[\n\t]', ' ', text)
         text = re.sub('<.*?/>', '', text)
-
-
         text = re.sub('\|? ?align=[a-z]+', '', text)
         text = re.sub('\|? ?valign=[a-z]+', '', text)
         text = re.sub('\|? ?scope=[a-z]+', '', text)
-        
+        text = re.sub('&lt;ref&gt;.*?&lt;/ref&gt;', ' ', text) 
+        text = re.sub('&lt;.*?&gt;', ' ', text)
+        text = re.sub('File:[A-Za-z0-9 ]+\.[a-z]{3,4}(\|[0-9]+px)?', '', text)
+        text = re.sub('Source: \[.*?\]', '', text)
         text = text.replace("Country flag |", "country:")
         text = text.replace("flag |", "country:")
         text = text.replace("flagicon |", "country:")
@@ -290,12 +275,7 @@ if __name__ == '__main__':
         text = text.replace("abbr=on", "")
         text = text.replace("disp=table", "")
         text = text.replace("sortname |", "")
-
-        text = re.sub('&lt;ref&gt;.*?&lt;/ref&gt;', ' ', text) 
-        text = re.sub('&lt;.*?&gt;', ' ', text)
-        text = re.sub('File:[A-Za-z0-9 ]+\.[a-z]{3,4}(\|[0-9]+px)?', '', text)
-        
-        text = re.sub('Source: \[.*?\]', '', text)
+ 
         texts.append(Text(text))
         
         document = document.replace("\n", " ").replace("\t", " ")
@@ -306,6 +286,7 @@ if __name__ == '__main__':
             text = segment.text.replace("\n", " ").replace("\t", " ")
             f1.write(str(id1) + '\t' + text + '\t' + document + '\n')
             id1+=1
+
         segments = SegmentProcessor.segment(texts, seg_size=8, stride=4).segments
         for segment in segments:
             if segment.text is None:
--- scripts/wiki_generate_tsv_no_tables_lists.py
+++ scripts/wiki_generate_tsv_no_tables_lists.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 import argparse
 from tqdm import tqdm
 import sqlite3
@@ -22,8 +21,11 @@ from pygaggle.rerank.base import Text
 from pygaggle.data.segmentation import SegmentProcessor
 import spacy
 
+'''After WikiExtractor and https://github.com/facebookresearch/DrQA/tree/main/scripts/retriever pre-processing is done on the XML dump, 
+the final pre-processing is done in this script to generate the WIKI_6_3, WIKI_8_4, and WIKI_100w corpuses (without tables and lists) '''
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='generate .tsv files for wiki corpuses without tables and lists')
+    parser = argparse.ArgumentParser(description='generate .tsv files for wiki corpuses of passages without tables and lists')
     parser.add_argument('-db_path', type=str, required=True, help='path to .db file containing preprocessed wiki pages from DrQA')
     parser.add_argument('-output_path_6_3', type=str, default="../collections/wiki_6_3.tsv", help='path to write .tsv with segment 6, stride 3')
     parser.add_argument('-output_path_8_4', type=str, default="../collections/wiki_8_4.tsv", help='path to write .tsv with segment 8, stride 4')
@@ -32,7 +34,6 @@ if __name__ == '__main__':
 
     sqliteConnection = sqlite3.connect(args.db_path)
     cursor = sqliteConnection.cursor()
-
     sqlite_select_query = "SELECT id, text FROM documents"
     cursor.execute(sqlite_select_query)
     pages = cursor.fetchall()
@@ -42,8 +43,9 @@ if __name__ == '__main__':
 
     documents = {}
 
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_lg")
 
+    # To avoid duplicate pages
     for row in tqdm(pages):
         text = row[1]
         title = row[0]
@@ -68,10 +70,12 @@ if __name__ == '__main__':
         texts = []
         text = documents[document]
         text = text.strip()
+
         if text.startswith("REDIRECT") or text.startswith("redirect"):
             continue
         if text.endswith(". References."):
-            text = text[:-len(". References.")].strip()
+            text = text[:-len(" References.")].strip()
+            
         text = re.sub('\{\{cite .*?\}\}', ' ', text, flags=re.DOTALL) 
         text = text.replace(r"TABLETOREPLACE", " ")
         text = text.replace(r"'''", " ")
@@ -87,7 +91,7 @@ if __name__ == '__main__':
         text = text.replace("formatnum:", "")
         
         #text = re.sub('<poem.*?</poem>', ' ', text, flags=re.DOTALL) # might have useful information?
-        text = re.sub('<math.*?</math>', '', text, flags=re.DOTALL) # is in latex could maybe parse into normal text?
+        text = re.sub('<math.*?</math>', '', text, flags=re.DOTALL)
         text = re.sub('<chem.*?</chem>', '', text, flags=re.DOTALL) 
         text = re.sub('<score.*?</score>', '', text, flags=re.DOTALL) 
 
@@ -106,8 +110,7 @@ if __name__ == '__main__':
         text = re.sub('\| ?ul_?style= ?.*? ', ' ', text)
         text = re.sub('\| ?li_?style= ?.*? ', ' ', text)
         text = re.sub('\| ?border-style= ?.*? ', ' ', text)
-    
-        text = re.sub('\|? ?style=\".*?\"', '', text) 
+        text = re.sub('\|? ?style=\".*?\"', '', text)
         text = re.sub('\|? ?rowspan=\".*?\"', '', text)
         text = re.sub('\|? ?colspan=\".*?\"', '', text)
         text = re.sub('\|? ?scope=\".*?\"', '', text)
@@ -117,19 +120,19 @@ if __name__ == '__main__':
         text = re.sub('\|? ?bgcolor=\".*?\"', '', text)
         text = re.sub('\|? ?bg=\#[a-z]+', '', text)
         text = re.sub('\|? ?width=\".*?\"', '', text)
-
         text = re.sub('\|? ?height=[0-9]+', '', text)
         text = re.sub('\|? ?width=[0-9]+', '', text)
         text = re.sub('\|? ?rowspan=[0-9]+', '', text)
         text = re.sub('\|? ?colspan=[0-9]+', '', text)
-
         text = re.sub(r'[\n\t]', ' ', text)
         text = re.sub('<.*?/>', '', text)
-
         text = re.sub('\|? ?align=[a-z]+', '', text)
         text = re.sub('\|? ?valign=[a-z]+', '', text)
         text = re.sub('\|? ?scope=[a-z]+', '', text)
-        
+        text = re.sub('&lt;ref&gt;.*?&lt;/ref&gt;', ' ', text)
+        text = re.sub('&lt;.*?&gt;', ' ', text)
+        text = re.sub('File:[A-Za-z0-9 ]+\.[a-z]{3,4}(\|[0-9]+px)?', '', text)
+        text = re.sub('Source: \[.*?\]', '', text)     
         text = text.replace("Country flag|", "country:")
         text = text.replace("flag|", "country:")
         text = text.replace("flagicon|", "country:")
@@ -139,25 +142,18 @@ if __name__ == '__main__':
         text = text.replace("display=it", "")
         text = text.replace("abbr=on", "")
         text = text.replace("disp=table", "")
-
-        text = re.sub('&lt;ref&gt;.*?&lt;/ref&gt;', ' ', text) 
-        text = re.sub('&lt;.*?&gt;', ' ', text)
-        text = re.sub('File:[A-Za-z0-9 ]+\.[a-z]{3,4}(\|[0-9]+px)?', '', text)
         
-        text = re.sub('Source: \[.*?\]', '', text)
-
-
         texts.append(Text(text))
-            
+
         document = document.replace("\n", " ").replace("\t", " ")
         segments = SegmentProcessor.segment(texts, seg_size=6, stride=3).segments
         for segment in segments:
             if segment.text is None:
                 continue
             text = segment.text.replace("\n", " ").replace("\t", " ")
-           
             f1.write(str(id1) + '\t' + text + '\t' + document + '\n')
             id1+=1
+
         segments = SegmentProcessor.segment(texts, seg_size=8, stride=4).segments
         for segment in segments:
             if segment.text is None:
@@ -165,25 +161,35 @@ if __name__ == '__main__':
             text = segment.text.replace("\n", " ").replace("\t", " ")
             f2.write(str(id2) + '\t' + text + '\t' + document + '\n')
             id2+=1
+        
         full_text = ""
         for text in texts:
             full_text += text.text + " "
-    
         doc = nlp.make_doc(full_text)
-
         segments = []
         word_count = 0
         segment_tokens = []
         for token in doc:
             segment_tokens.append(token.text_with_ws)
-            if token.is_alpha:
+            if not token.is_space and not token.is_punct:
                 word_count+=1
                 if word_count == 100:
                     word_count = 0
                     segments.append(''.join([token for token in segment_tokens]))
                     segment_tokens = []
         if word_count != 0:
+            for token in doc:
+                segment_tokens.append(token.text_with_ws)
+                if not token.is_space and not token.is_punct:
+                    word_count+=1
+                    if word_count == 100:
+                        word_count = 0
+                        segments.append(''.join([token for token in segment_tokens]))
+                        break
+        if word_count != 0:
             segments.append(''.join([token for token in segment_tokens]))
+        if len(segments) > 0:
+            segments[0] = document + " " + segments[0]
         for segment in segments:
             text = segment.replace("\n", " ").replace("\t", " ")
             f3.write(str(id3) + '\t' + text + '\t' + document + '\n')
