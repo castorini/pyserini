b54dcb0ef041e15573f64db621f28b51cafbb10b
Jimmy Lin <jimmylin@uwaterloo.ca>
2022-10-29 16:22:22 -0400

    Refactor run_all_miracl.py to properly run train and dev sets (#1327)

--- scripts/repro_matrix/run_all_miracl.py
+++ scripts/repro_matrix/run_all_miracl.py
@@ -15,9 +15,9 @@
 #
 
 import argparse
-import subprocess
 import math
 import os
+import subprocess
 import time
 from collections import defaultdict
 from string import Template
@@ -25,7 +25,7 @@ from string import Template
 import yaml
 
 from scripts.repro_matrix.defs_miracl import models, languages, trec_eval_metric_definitions
-from scripts.repro_matrix.utils import run_eval_and_return_metric, ok_str, okish_str, fail_str
+from scripts.repro_matrix.utils import run_eval_and_return_metric, ok_str, fail_str
 
 
 def print_results(metric, split):
@@ -66,6 +66,7 @@ if __name__ == '__main__':
             cmd_template = condition['command']
 
             print(f'condition {name}:')
+            lang = name.split('.')[-1]
 
             for splits in condition['splits']:
                 split = splits['split']
@@ -74,6 +75,14 @@ if __name__ == '__main__':
 
                 runfile = f'runs/run.miracl.{name}.{split}.txt'
                 cmd = Template(cmd_template).substitute(split=split, output=runfile)
+
+                # In the yaml file, the topics are written as something like '--topics miracl-v1.0-ar-${split}'
+                # This works for the dev split because the topics are directly included in Anserini/Pyserini.
+                # For this training split, we have to map the symbol into a file in tools/topics-and-qrels/
+                # Here, we assume that the developer has cloned the miracl repo and placed the topics there.
+                if split == 'train':
+                    cmd = cmd.replace(f'--topics miracl-v1.0-{lang}-{split}',
+                                      f'--topics tools/topics-and-qrels/topics.miracl-v1.0-{lang}-{split}.tsv')
 
                 if not os.path.exists(runfile):
                     print(f'    Running: {cmd}')
@@ -87,18 +96,17 @@ if __name__ == '__main__':
                 for expected in splits['scores']:
                     for metric in expected:
                         if not args.skip_eval:
-                            score = float(run_eval_and_return_metric(metric, f'{eval_key}-{split}',
+                            # We have the translate the training qrels into a file located in tools/topics-and-qrels/
+                            # because they are not included with Anserini/Pyserini by default.
+                            # Here, we assume that the developer has cloned the miracl repo and placed the qrels there.
+                            if split == 'train':
+                                qrels = f'tools/topics-and-qrels/qrels.{eval_key}-train.tsv'
+                            else:
+                                qrels = f'{eval_key}-{split}'
+                            score = float(run_eval_and_return_metric(metric, qrels,
                                                                      trec_eval_metric_definitions[metric], runfile))
                             if math.isclose(score, float(expected[metric])):
                                 result_str = ok_str
-                            # Flaky test: small difference on orca
-                            elif name == 'mdpr-tied-pft-nq.te' and split == 'dev' \
-                                    and math.isclose(score, float(expected[metric]), abs_tol=2e-4):
-                                result_str = okish_str
-                            # Flaky test: small difference on orca
-                            elif name == 'mdpr-tied-pft-msmarco-ft-all.ko' and split == 'train' \
-                                    and math.isclose(score, float(expected[metric]), abs_tol=4e-4):
-                                result_str = okish_str
                             else:
                                 result_str = fail_str + f' expected {expected[metric]:.4f}'
                             print(f'      {metric:7}: {score:.4f} {result_str}')
