fc5de1af271d08a98d17d7bc4f66f4518fda7f90
Jimmy Lin <jimmylin@uwaterloo.ca>
2022-11-05 14:16:23 -0400

    Fix index stats for recently installed pre-built indexes (#1335)
    Bug as a result of #1334

--- pyserini/prebuilt_index_info.py
+++ pyserini/prebuilt_index_info.py
@@ -51,9 +51,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "b2b1841c93255f9902150128d5e27e41",
         "size compressed (bytes)": 13736982438,
-        "total_terms": 2742209690,
+        "total_terms": 2742219865,
         "documents": 3213835,
-        "unique_terms": 29820456,
+        "unique_terms": 29823777,
         "downloaded": False
     },
     "msmarco-v1-doc-slim": {
@@ -62,13 +62,12 @@ TF_INDEX_INFO_CURRENT = {
         "readme": "lucene-index.msmarco-v1-doc.20221004.252b5e.README.md",
         "urls": [
             "https://rgw.cs.uwaterloo.ca/JIMMYLIN-bucket0/pyserini-indexes/lucene-index.msmarco-v1-doc-slim.20221004.252b5e.tar.gz",
-            "https://vault.cs.uwaterloo.ca/s/iCnysqnaG9SL9pA/download"
         ],
         "md5": "400fe94ec97a20cf775596085c5ad79d",
         "size compressed (bytes)": 1791498133,
-        "total_terms": 2742209690,
+        "total_terms": 2742219865,
         "documents": 3213835,
-        "unique_terms": 29820456,
+        "unique_terms": 29823777,
         "downloaded": False
     },
     "msmarco-v1-doc-full": {
@@ -80,9 +79,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "75735da0dd35e3631d22bf682ebed8a0",
         "size compressed (bytes)": 25525615599,
-        "total_terms": 2742209690,
+        "total_terms": 2742219865,
         "documents": 3213835,
-        "unique_terms": 29820456,
+        "unique_terms": 29823777,
         "downloaded": False
     },
 
@@ -96,9 +95,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "87530b64e55080fcfb90ec9e598be23e",
         "size compressed (bytes)": 1885596544,
-        "total_terms": 3748333319,
+        "total_terms": 3748343494,
         "documents": 3213835,
-        "unique_terms": 30627687,
+        "unique_terms": 30631009,
         "downloaded": False
     },
     "msmarco-v1-doc-d2q-t5-docvectors": {
@@ -110,9 +109,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "a081b866b78e0f604ddb9e3103ee6cc5",
         "size compressed (bytes)": 11152231182,
-        "total_terms": 3748333319,
+        "total_terms": 3748343494,
         "documents": 3213835,
-        "unique_terms": 30627687,
+        "unique_terms": 30631009,
         "downloaded": False
     },
 
@@ -126,9 +125,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "59fdf88f360d0a72d1b94b9729c2198e",
         "size compressed (bytes)": 15924438098,
-        "total_terms": 3200515914,
+        "total_terms": 3200522554,
         "documents": 20545677,
-        "unique_terms": 21190687,
+        "unique_terms": 21191748,
         "downloaded": False
     },
     "msmarco-v1-doc-segmented-slim": {
@@ -140,9 +139,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "c277161780d501ab832e16e6396f9cae",
         "size compressed (bytes)": 3306727108,
-        "total_terms": 3200515914,
+        "total_terms": 3200522554,
         "documents": 20545677,
-        "unique_terms": 21190687,
+        "unique_terms": 21191748,
         "downloaded": False
     },
     "msmarco-v1-doc-segmented-full": {
@@ -154,9 +153,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "c1af97d16c552a99a23382639c4a668c",
         "size compressed (bytes)": 29470600011,
-        "total_terms": 3200515914,
+        "total_terms": 3200522554,
         "documents": 20545677,
-        "unique_terms": 21190687,
+        "unique_terms": 21191748,
         "downloaded": False
     },
 
@@ -170,9 +169,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "b242fd9cb0982e87d0c667439cb6d59c",
         "size compressed (bytes)": 3554554620,
-        "total_terms": 4206639543,
+        "total_terms": 4206646183,
         "documents": 20545677,
-        "unique_terms": 22054207,
+        "unique_terms": 22055268,
         "downloaded": False
     },
     "msmarco-v1-doc-segmented-d2q-t5-docvectors": {
@@ -184,9 +183,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "40341fc2cf151b8c447a8e77f5e9f100",
         "size compressed (bytes)": 16349673687,
-        "total_terms": 4206639543,
+        "total_terms": 4206646183,
         "documents": 20545677,
-        "unique_terms": 22054207,
+        "unique_terms": 22055268,
         "downloaded": False
     },
 
@@ -318,9 +317,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "0599bd6ed5ee28390b279eb398ef0267",
         "size compressed (bytes)": 63431299815,
-        "total_terms": 14165661202,
+        "total_terms": 14165667143,
         "documents": 11959635,
-        "unique_terms": 44855557,
+        "unique_terms": 44860768,
         "downloaded": False
     },
     "msmarco-v2-doc-slim": {
@@ -332,9 +331,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "4dfc5549e3c15abec4b9694542a376d1",
         "size compressed (bytes)": 7172175394,
-        "total_terms": 14165661202,
+        "total_terms": 14165667143,
         "documents": 11959635,
-        "unique_terms": 44855557,
+        "unique_terms": 44860768,
         "downloaded": False
     },
     "msmarco-v2-doc-full": {
@@ -346,9 +345,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "fc6f546898725617eb5ca7a144bef531",
         "size compressed (bytes)": 119537276117,
-        "total_terms": 14165661202,
+        "total_terms": 14165667143,
         "documents": 11959635,
-        "unique_terms": 44855557,
+        "unique_terms": 44860768,
         "downloaded": False
     },
 
@@ -362,9 +361,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "25514f77600a6be87aeb1c66c9107b89",
         "size compressed (bytes)": 8155218407,
-        "total_terms": 19760777295,
+        "total_terms": 19760783236,
         "documents": 11959635,
-        "unique_terms": 54143053,
+        "unique_terms": 54148271,
         "downloaded": False
     },
     "msmarco-v2-doc-d2q-t5-docvectors": {
@@ -376,9 +375,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "a3ce9b1146857a332825825623ab89e7",
         "size compressed (bytes)": 54415612794,
-        "total_terms": 19760777295,
+        "total_terms": 19760783236,
         "documents": 11959635,
-        "unique_terms": 54143053,
+        "unique_terms": 54148271,
         "downloaded": False
     },
 
@@ -392,9 +391,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "8a5f444fa5a63cc5d4ddc3e6dd15faa0",
         "size compressed (bytes)": 109269078191,
-        "total_terms": 24780915974,
+        "total_terms": 24780918039,
         "documents": 124131414,
-        "unique_terms": 29263590,
+        "unique_terms": 29265408,
         "downloaded": False
     },
     "msmarco-v2-doc-segmented-slim": {
@@ -406,9 +405,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "f50c591aa9a0a0126ebc4dc53c6306d7",
         "size compressed (bytes)": 20852487058,
-        "total_terms": 24780915974,
+        "total_terms": 24780918039,
         "documents": 124131414,
-        "unique_terms": 29263590,
+        "unique_terms": 29265408,
         "downloaded": False
     },
     "msmarco-v2-doc-segmented-full": {
@@ -420,9 +419,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "259b936d3591e48770da9dde153d1617",
         "size compressed (bytes)": 201358944352,
-        "total_terms": 24780915974,
+        "total_terms": 24780918039,
         "documents": 124131414,
-        "unique_terms": 29263590,
+        "unique_terms": 29265408,
         "downloaded": False
     },
 
@@ -436,9 +435,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "1e9fa18f082aaadfef02ba9eea32fcc2",
         "size compressed (bytes)": 24242738999,
-        "total_terms": 30376032067,
+        "total_terms": 30376034132,
         "documents": 124131414,
-        "unique_terms": 38930475,
+        "unique_terms": 38932296,
         "downloaded": False
     },
     "msmarco-v2-doc-segmented-d2q-t5-docvectors": {
@@ -450,9 +449,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "eff6fe5b61936491c8985ad7efa46b20",
         "size compressed (bytes)": 114315186555,
-        "total_terms": 30376032067,
+        "total_terms": 30376034132,
         "documents": 124131414,
-        "unique_terms": 38930475,
+        "unique_terms": 38932296,
         "downloaded": False
     },
 
@@ -466,9 +465,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "eacd8556dd416ccad517b5e7dc97bceb",
         "size compressed (bytes)": 38808092190,
-        "total_terms": 4673266762,
+        "total_terms": 4673266800,
         "documents": 138364198,
-        "unique_terms": 11885026,
+        "unique_terms": 11885838,
         "downloaded": False
     },
     "msmarco-v2-passage-slim": {
@@ -480,9 +479,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "d7e644c048669aa72314dd358b475765",
         "size compressed (bytes)": 8170344330,
-        "total_terms": 4673266762,
+        "total_terms": 4673266800,
         "documents": 138364198,
-        "unique_terms": 11885026,
+        "unique_terms": 11885838,
         "downloaded": False
     },
     "msmarco-v2-passage-full": {
@@ -494,9 +493,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "ef5c22c865094c386b9ec600165bb061",
         "size compressed (bytes)": 60413585958,
-        "total_terms": 4673266762,
+        "total_terms": 4673266800,
         "documents": 138364198,
-        "unique_terms": 11885026,
+        "unique_terms": 11885838,
         "downloaded": False
     },
 
@@ -510,9 +509,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "3c357f9c219e4c3d980bc663e1f5a5f4",
         "size compressed (bytes)": 14404903785,
-        "total_terms": 16961479226,
+        "total_terms": 16961479264,
         "documents": 138364198,
-        "unique_terms": 36650715,
+        "unique_terms": 36651533,
         "downloaded": False
     },
     "msmarco-v2-passage-d2q-t5-docvectors": {
@@ -524,9 +523,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "01e369b644e5a8b7413e04140780cf94",
         "size compressed (bytes)": 59206472740,
-        "total_terms": 16961479226,
+        "total_terms": 16961479264,
         "documents": 138364198,
-        "unique_terms": 36650715,
+        "unique_terms": 36651533,
         "downloaded": False
     },
 
@@ -540,9 +539,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "69675971a0172eb5e37668ea42761d43",
         "size compressed (bytes)": 75036026507,
-        "total_terms": 15272964956,
+        "total_terms": 15272965252,
         "documents": 138364198,
-        "unique_terms": 16579071,
+        "unique_terms": 16579899,
         "downloaded": False
     },
     "msmarco-v2-passage-augmented-slim": {
@@ -554,9 +553,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "3524b5b28117ac1a5365cd664c6871f1",
         "size compressed (bytes)": 14757394934,
-        "total_terms": 15272964956,
+        "total_terms": 15272965252,
         "documents": 138364198,
-        "unique_terms": 16579071,
+        "unique_terms": 16579899,
         "downloaded": False
     },
     "msmarco-v2-passage-augmented-full": {
@@ -568,9 +567,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "c3e18c02e749c0416e1acc653899c6b0",
         "size compressed (bytes)": 130622740320,
-        "total_terms": 15272964956,
+        "total_terms": 15272965252,
         "documents": 138364198,
-        "unique_terms": 16579071,
+        "unique_terms": 16579899,
         "downloaded": False
     },
 
@@ -584,9 +583,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "2b683a3a64692b95375ddbdcb9590f25",
         "size compressed (bytes)": 14404903785,
-        "total_terms": 27561177420,
+        "total_terms": 27561177716,
         "documents": 138364198,
-        "unique_terms": 41176227,
+        "unique_terms": 41177061,
         "downloaded": False
     },
     "msmarco-v2-passage-augmented-d2q-t5-docvectors": {
@@ -598,9 +597,9 @@ TF_INDEX_INFO_CURRENT = {
         ],
         "md5": "fe6eaeceabaa06cb09fdf8432f65f9d8",
         "size compressed (bytes)": 59206472740,
-        "total_terms": 27561177420,
+        "total_terms": 27561177716,
         "documents": 138364198,
-        "unique_terms": 41176227,
+        "unique_terms": 41177061,
         "downloaded": False
     },
 
