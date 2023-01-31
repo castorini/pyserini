21350ceacf9840f3f8222f7f02875761a41d758e
lintool <jimmylin@uwaterloo.ca>
2023-01-31 07:47:29 -0500

    Increase tolerance for flaky LTR test.

--- integrations/sparse/test_lucenesearcher_check_ltr_msmarco_document.py
+++ integrations/sparse/test_lucenesearcher_check_ltr_msmarco_document.py
@@ -50,8 +50,10 @@ class TestLtrMsmarcoDocument(unittest.TestCase):
         result = subprocess.check_output(f'python tools/scripts/msmarco/msmarco_doc_eval.py --judgments tools/topics-and-qrels/qrels.msmarco-doc.dev.txt --run ltr_test/{outp}', shell=True).decode(sys.stdout.encoding)
         a,b = result.find('#####################\nMRR @100:'), result.find('\nQueriesRanked: 5193\n#####################\n')
         mrr = result[a+32:b]
-        # See https://github.com/castorini/pyserini/issues/951
-        self.assertAlmostEqual(float(mrr), 0.3108, delta=0.0001)
+        # See:
+        #  - https://github.com/castorini/pyserini/issues/951
+        #  - https://github.com/castorini/pyserini/issues/1430
+        self.assertAlmostEqual(float(mrr), 0.3108, delta=0.0002)
         rmtree('ltr_test')
 
 if __name__ == '__main__':
