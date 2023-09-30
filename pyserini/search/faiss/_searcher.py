679b153482357562c4f1b3ccb6499122c34e6d76
Jimmy Lin <jimmylin@uwaterloo.ca>
2023-09-30 08:24:51 -0400

    Revert "Add remove_dups argument for dense retrieval (#1510)"
    This reverts commit d0bccfecc66f7d5f7f3f3a50e4de4ac909f30eac.

--- pyserini/search/faiss/_searcher.py
+++ pyserini/search/faiss/_searcher.py
@@ -439,7 +439,7 @@ class FaissSearcher:
         """Display information about available prebuilt indexes."""
         get_dense_indexes_info()
 
-    def search(self, query: Union[str, np.ndarray], k: int = 10, threads: int = 1, remove_dups: bool = False, return_vector: bool = False) \
+    def search(self, query: Union[str, np.ndarray], k: int = 10, threads: int = 1, return_vector: bool = False) \
             -> Union[List[DenseSearchResult], Tuple[np.ndarray, List[PRFDenseSearchResult]]]:
         """Search the collection.
 
@@ -451,8 +451,6 @@ class FaissSearcher:
             Number of hits to return.
         threads : int
             Maximum number of threads to use for intra-query search.
-        remove_dups : bool
-            Remove duplicate docids when writing final run output.    
         return_vector : bool
             Return the results with vectors
         Returns
@@ -479,14 +477,6 @@ class FaissSearcher:
             distances, indexes = self.index.search(emb_q, k)
             distances = distances.flat
             indexes = indexes.flat
-            if remove_dups == True:
-                unique_docs = set()
-                results = list()
-                for score, idx in zip(distances, indexes):
-                if idx not in unique_docs:
-                    unique_docs.add(idx)
-                    results.append(DenseSearchResult(self.docids[idx],score))
-                return results
             return [DenseSearchResult(self.docids[idx], score)
                     for score, idx in zip(distances, indexes) if idx != -1]
 
