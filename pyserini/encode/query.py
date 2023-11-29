723e06c3b04e6c6fcd56fcf5bce4386c72503e5a
Xueguang Ma 马雪光 <x93ma@edu.uwaterloo.ca>
2023-11-28 20:18:27 -0500

    config pooling and l2norm for autoencoder (#1722)
    * config pooling and l2norm for autoencoder
    * config prefix for query and document encoder

--- pyserini/encode/query.py
+++ pyserini/encode/query.py
@@ -24,7 +24,7 @@ from pyserini.encode import DprQueryEncoder, TctColBertQueryEncoder, AnceQueryEn
 from pyserini.encode import UniCoilQueryEncoder, SpladeQueryEncoder, OpenAIQueryEncoder
 
 
-def init_encoder(encoder, device):
+def init_encoder(encoder, device, pooling, l2_norm, prefix):
     if 'dpr' in encoder.lower():
         return DprQueryEncoder(encoder, device=device)
     elif 'tct' in encoder.lower():
@@ -40,7 +40,7 @@ def init_encoder(encoder, device):
     elif 'openai-api' in encoder.lower():
         return OpenAIQueryEncoder()
     else:
-        return AutoQueryEncoder(encoder, device=device)
+        return AutoQueryEncoder(encoder, device=device, pooling=pooling, l2_norm=l2_norm, prefix=prefix)
 
 
 if __name__ == '__main__':
@@ -54,9 +54,14 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, help='device cpu or cuda [cuda:0, cuda:1...]',
                         default='cpu', required=False)
     parser.add_argument('--max-length', type=int, help='max length', default=256, required=False)
+    parser.add_argument('--pooling', type=str, help='pooling strategy', default='cls', choices=['cls', 'mean'],
+                        required=False)
+    parser.add_argument('--l2-norm', action='store_true', help='whether to normalize embedding', default=False,
+                        required=False)
+    parser.add_argument('--prefx', type=str, help='prefix query input', default=None, required=False)
     args = parser.parse_args()
 
-    encoder = init_encoder(args.encoder, device=args.device)
+    encoder = init_encoder(args.encoder, device=args.device, pooling=args.pooling, l2_norm=args.l2_norm, prefix=args.prefx)
     query_iterator = DefaultQueryIterator.from_topics(args.topics)
 
     is_sparse = False
--- pyserini/search/faiss/__main__.py
+++ pyserini/search/faiss/__main__.py
@@ -47,6 +47,11 @@ def define_dsearch_args(parser):
     parser.add_argument('--encoder', type=str, metavar='path to query encoder checkpoint or encoder name',
                         required=False,
                         help="Path to query encoder pytorch checkpoint or hgf encoder model name")
+    parser.add_argument('--pooling', type=str, metavar='pooling strategy', required=False, default='cls',
+                        choices=['cls', 'mean'],
+                        help="Pooling strategy for query encoder")
+    parser.add_argument('--l2-norm', action='store_true', help='whether to normalize embedding', default=False,
+                        required=False)
     parser.add_argument('--tokenizer', type=str, metavar='name or path',
                         required=False,
                         help="Path to a hgf tokenizer name or path")
@@ -85,7 +90,7 @@ def define_dsearch_args(parser):
                         help="Set efSearch for HNSW index")
 
 
-def init_query_encoder(encoder, encoder_class, tokenizer_name, topics_name, encoded_queries, device, prefix, max_length):
+def init_query_encoder(encoder, encoder_class, tokenizer_name, topics_name, encoded_queries, device, max_length, pooling, l2_norm, prefix):
     encoded_queries_map = {
         'msmarco-passage-dev-subset': 'tct_colbert-msmarco-passage-dev-subset',
         'dpr-nq-dev': 'dpr_multi-nq-dev',
@@ -126,6 +131,7 @@ def init_query_encoder(encoder, encoder_class, tokenizer_name, topics_name, enco
             # if none of the class keyword was matched,
             # use the AutoQueryEncoder
             if encoder_class is None:
+                _encoder_class = "auto"
                 encoder_class = AutoQueryEncoder
 
         # prepare arguments to encoder class
@@ -136,6 +142,8 @@ def init_query_encoder(encoder, encoder_class, tokenizer_name, topics_name, enco
             kwargs.update(dict(pooling='mean', l2_norm=False))
         if (_encoder_class == "openai-api") or ("openai" in encoder):
             kwargs.update(dict(max_length=max_length))
+        if (_encoder_class == "auto"):
+            kwargs.update(dict(pooling=pooling, l2_norm=l2_norm, prefix=prefix))
         return encoder_class(**kwargs)
 
     if encoded_queries:
@@ -188,7 +196,7 @@ if __name__ == '__main__':
     topics = query_iterator.topics
 
     query_encoder = init_query_encoder(
-        args.encoder, args.encoder_class, args.tokenizer, args.topics, args.encoded_queries, args.device, args.query_prefix, args.max_length)
+        args.encoder, args.encoder_class, args.tokenizer, args.topics, args.encoded_queries, args.device, args.max_length, args.pooling, args.l2_norm, args.query_prefix)
     if args.pca_model:
         query_encoder = PcaEncoder(query_encoder, args.pca_model)
     kwargs = {}
