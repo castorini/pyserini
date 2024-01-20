bc70c24f3e53266bef6491b250c9e17f0c3f4ba7
AlexStan0 <stan_alexandru06@yahoo.com>
2024-01-20 17:14:48 -0500

    added reproduction logs

---
+++ data_munging.py
@@ -0,0 +1,7 @@
+import json
+
+with open('collections/nfcorpus/queries.tsv', 'w') as out:
+        with open('collections/nfcorpus/queries.jsonl', 'r') as f:
+            for line in f:
+                l = json.loads(line)
+                out.write(l['_id'] + '\t' + l['text'] + '\n')
