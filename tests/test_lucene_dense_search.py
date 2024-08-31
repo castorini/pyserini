56ec029d7bc1dea55c7a174dcfd4929b806511b4
lintool <jimmylin@uwaterloo.ca>
2024-08-31 16:49:55 -0400

    Add bindings to Lucene's HNSW and flat dense searcher: batch search ONNX on BEIR

--- tests/test_lucene_dense_search.py
+++ tests/test_lucene_dense_search.py
@@ -72,6 +72,52 @@ class TestLuceneDenseSearch(unittest.TestCase):
         self.assertAlmostEqual(hits[3].score, 0.888884, places=5)
         self.assertAlmostEqual(hits[4].score, 0.883876, places=5)
 
+    def test_lucene_hnsw_dense_searcher_batch(self):
+        searcher = LuceneHnswDenseSearcher.from_prebuilt_index(
+            'beir-v1.0.0-arguana.bge-base-en-v1.5.hnsw', encoder='BgeBaseEn15')
+        topics = get_topics('beir-v1.0.0-arguana-test')
+        qids = ['test-culture-ahrtsdlgra-con01a', 'test-culture-ahrtsdlgra-con02a']
+        queries = [topics[qids[0]]['title'], topics[qids[1]]['title']]
+
+        results = searcher.batch_search(queries, qids, k=5, threads=2)
+
+        # Ground truth results
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-con03a 1 0.893029 Anserini
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-con01b 2 0.892179 Anserini
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-pro02b 3 0.888884 Anserini
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-pro02a 4 0.883876 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 training-free-speech-debate-nvhsibsv-con01a 1 0.886786 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 training-digital-freedoms-piidfphwbaa-con01b 2 0.878777 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 training-digital-freedoms-fehwbawdh-con01a 3 0.876869 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 test-culture-ahrtsdlgra-con02b 4 0.876068 Anserini
+
+        self.assertEqual(len(results), 2)
+        self.assertEqual(len(results['test-culture-ahrtsdlgra-con01a']), 5)
+        self.assertEqual(len(results['test-culture-ahrtsdlgra-con02a']), 5)
+
+        hits = results['test-culture-ahrtsdlgra-con01a']
+        # First query: skip first hit, because it's just the query.
+        self.assertEqual(hits[1].docid, 'test-culture-ahrtsdlgra-con03a')
+        self.assertEqual(hits[2].docid, 'test-culture-ahrtsdlgra-con01b')
+        self.assertEqual(hits[3].docid, 'test-culture-ahrtsdlgra-pro02b')
+        self.assertEqual(hits[4].docid, 'test-culture-ahrtsdlgra-pro02a')
+
+        self.assertAlmostEqual(hits[1].score, 0.893029, places=5)
+        self.assertAlmostEqual(hits[2].score, 0.892179, places=5)
+        self.assertAlmostEqual(hits[3].score, 0.888884, places=5)
+        self.assertAlmostEqual(hits[4].score, 0.883876, places=5)
+
+        hits = results['test-culture-ahrtsdlgra-con02a']
+        self.assertEqual(hits[1].docid, 'training-free-speech-debate-nvhsibsv-con01a')
+        self.assertEqual(hits[2].docid, 'training-digital-freedoms-piidfphwbaa-con01b')
+        self.assertEqual(hits[3].docid, 'training-digital-freedoms-fehwbawdh-con01a')
+        self.assertEqual(hits[4].docid, 'test-culture-ahrtsdlgra-con02b')
+
+        self.assertAlmostEqual(hits[1].score, 0.886786, places=5)
+        self.assertAlmostEqual(hits[2].score, 0.878777, places=5)
+        self.assertAlmostEqual(hits[3].score, 0.876869, places=5)
+        self.assertAlmostEqual(hits[4].score, 0.876068, places=5)
+
     def test_lucene_flat_dense_searcher(self):
         searcher = LuceneFlatDenseSearcher.from_prebuilt_index(
             'beir-v1.0.0-arguana.bge-base-en-v1.5.flat', encoder='BgeBaseEn15')
@@ -120,6 +166,52 @@ class TestLuceneDenseSearch(unittest.TestCase):
         self.assertAlmostEqual(hits[2].score, 0.892179, places=5)
         self.assertAlmostEqual(hits[3].score, 0.888884, places=5)
         self.assertAlmostEqual(hits[4].score, 0.883876, places=5)
+
+    def test_lucene_flat_dense_searcher_batch(self):
+        searcher = LuceneFlatDenseSearcher.from_prebuilt_index(
+            'beir-v1.0.0-arguana.bge-base-en-v1.5.flat', encoder='BgeBaseEn15')
+        topics = get_topics('beir-v1.0.0-arguana-test')
+        qids = ['test-culture-ahrtsdlgra-con01a', 'test-culture-ahrtsdlgra-con02a']
+        queries = [topics[qids[0]]['title'], topics[qids[1]]['title']]
+
+        results = searcher.batch_search(queries, qids, k=5, threads=2)
+
+        # Ground truth results
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-con03a 1 0.893029 Anserini
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-con01b 2 0.892179 Anserini
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-pro02b 3 0.888884 Anserini
+        # test-culture-ahrtsdlgra-con01a Q0 test-culture-ahrtsdlgra-pro02a 4 0.883876 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 training-free-speech-debate-nvhsibsv-con01a 1 0.886786 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 training-digital-freedoms-piidfphwbaa-con01b 2 0.878777 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 training-digital-freedoms-fehwbawdh-con01a 3 0.876869 Anserini
+        # test-culture-ahrtsdlgra-con02a Q0 test-culture-ahrtsdlgra-con02b 4 0.876068 Anserini
+
+        self.assertEqual(len(results), 2)
+        self.assertEqual(len(results['test-culture-ahrtsdlgra-con01a']), 5)
+        self.assertEqual(len(results['test-culture-ahrtsdlgra-con02a']), 5)
+
+        hits = results['test-culture-ahrtsdlgra-con01a']
+        # First query: skip first hit, because it's just the query.
+        self.assertEqual(hits[1].docid, 'test-culture-ahrtsdlgra-con03a')
+        self.assertEqual(hits[2].docid, 'test-culture-ahrtsdlgra-con01b')
+        self.assertEqual(hits[3].docid, 'test-culture-ahrtsdlgra-pro02b')
+        self.assertEqual(hits[4].docid, 'test-culture-ahrtsdlgra-pro02a')
+
+        self.assertAlmostEqual(hits[1].score, 0.893029, places=5)
+        self.assertAlmostEqual(hits[2].score, 0.892179, places=5)
+        self.assertAlmostEqual(hits[3].score, 0.888884, places=5)
+        self.assertAlmostEqual(hits[4].score, 0.883876, places=5)
+
+        hits = results['test-culture-ahrtsdlgra-con02a']
+        self.assertEqual(hits[1].docid, 'training-free-speech-debate-nvhsibsv-con01a')
+        self.assertEqual(hits[2].docid, 'training-digital-freedoms-piidfphwbaa-con01b')
+        self.assertEqual(hits[3].docid, 'training-digital-freedoms-fehwbawdh-con01a')
+        self.assertEqual(hits[4].docid, 'test-culture-ahrtsdlgra-con02b')
+
+        self.assertAlmostEqual(hits[1].score, 0.886786, places=5)
+        self.assertAlmostEqual(hits[2].score, 0.878777, places=5)
+        self.assertAlmostEqual(hits[3].score, 0.876869, places=5)
+        self.assertAlmostEqual(hits[4].score, 0.876068, places=5)
 
 
 if __name__ == '__main__':
