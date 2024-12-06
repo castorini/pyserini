793f552dac9d4babb171f8c3f2c29f4a29285f06
sherlock <j586yang@uwaterloo.ca>
2024-12-05 22:45:17 -0500

    successful onboarding reproduction for pyserini

--- queryRepresentation.py
+++
@@ -1,7 +0,0 @@
-from pyserini.analysis import Analyzer, get_lucene_analyzer
-
-analyzer = Analyzer(get_lucene_analyzer())
-query_tokens = analyzer.analyze('what is paula deen\'s brother')
-multihot_query_weights = {k: 1 for k in query_tokens}
-
-print(multihot_query_weights)
