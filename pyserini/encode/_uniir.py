fab28d959baf8c90ac32dabe28fd0f87e3d5644a
clides <daniel168.guo@gmail.com>
2025-07-25 11:14:36 -0400

    add instructions injection for query embedding

--- pyserini/encode/_uniir.py
+++ pyserini/encode/_uniir.py
@@ -23,7 +23,7 @@ from typing import Any, List, Optional
 from types import SimpleNamespace
 from importlib.resources import files
 
-from pyserini import encode
+import pandas as pd
 import torch
 import faiss
 import pandas as pd
