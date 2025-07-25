49a508f646de389e588d0bda87a55b16d3667175
clides <daniel168.guo@gmail.com>
2025-07-25 16:59:58 -0400

    refractored uniir modules integration

--- pyserini/encode/_uniir.py
+++ pyserini/encode/_uniir.py
@@ -22,6 +22,7 @@ from typing import Any, List, Optional
 from types import SimpleNamespace
 from importlib.resources import files
 
+import pandas as pd
 import torch
 import faiss
 import pandas as pd
