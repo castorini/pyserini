a5b6492fe20265b6f4075f658ebf5072cc90ec44
lilyjge <lilyjge@gmail.com>
2025-07-31 10:07:32 -0400

    versioning for rest test routes

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -16,7 +16,7 @@
 
 import unittest
 from fastapi.testclient import TestClient
-from pyserini.server.rest.app import app
+from pyserini.server.rest.app import app, VERSION
 
 client = TestClient(app)
 
@@ -27,7 +27,7 @@ class TestRestServer(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_search(self):
-        response = client.get("/v1/indexes/msmarco-v1-passage/search?query=what%20is%20a%20lobster%20roll&hits=1")
+        response = client.get(f"/{VERSION}/indexes/msmarco-v1-passage/search?query=what%20is%20a%20lobster%20roll&hits=1")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("query" in list(response.json().keys()))
         self.assertTrue("candidates" in list(response.json().keys()))
@@ -35,26 +35,26 @@ class TestRestServer(unittest.TestCase):
         self.assertEqual("7157707", response.json().get("candidates")[0].get("docid"))
 
     def test_get_doc(self):
-        response = client.get("v1/indexes/msmarco-v1-passage/documents/7157707")
+        response = client.get(f"/{VERSION}/indexes/msmarco-v1-passage/documents/7157707")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("id" in list(response.json().keys()))
         self.assertTrue("contents" in list(response.json().keys()))
         self.assertTrue("Lobster Roll" in response.json().get("contents"))
 
     def test_index_status(self):
-        response = client.get("v1/indexes/msmarco-v1-passage/status")
+        response = client.get(f"/{VERSION}/indexes/msmarco-v1-passage/status")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("downloaded" in list(response.json().keys()))
         self.assertTrue("size_bytes" in list(response.json().keys()))
         self.assertTrue(response.json().get("downloaded"))
 
     def test_list_indexes(self):
-        response = client.get("v1/indexes/?index_type=tf")
+        response = client.get(f"/{VERSION}/indexes/?index_type=tf")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 196)
 
     def test_get_index_settings(self):
-        response = client.get("v1/indexes/msmarco-v1-passage/settings")
+        response = client.get(f"/{VERSION}/indexes/msmarco-v1-passage/settings")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("efSearch" in list(response.json().keys()))
         self.assertTrue("encoder" in list(response.json().keys()))
@@ -62,6 +62,6 @@ class TestRestServer(unittest.TestCase):
 
     def test_post_index_settings(self):
         data = {"efSearch": "100", "encoder": "BgeBaseEn15Encoder", "queryGenerator": "BagOfWordsGenerator"}
-        response = client.post("v1/indexes/msmarco-v1-passage/settings", json=data)
+        response = client.post(f"/{VERSION}/indexes/msmarco-v1-passage/settings", json=data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"status": "success"})
