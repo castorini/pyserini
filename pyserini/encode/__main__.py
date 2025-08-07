906024788b8c7bb3e31fd18c1759fe4be44f1584
clides <daniel168.guo@gmail.com>
2025-08-07 18:09:42 -0400

    fix rebase issues

--- pyserini/encode/__main__.py
+++ pyserini/encode/__main__.py
@@ -52,8 +52,6 @@ def init_encoder(encoder, encoder_class, device, pooling, l2_norm, prefix, multi
         kwargs.update(dict(pooling='mean', l2_norm=False))
     if _encoder_class == 'auto':
         kwargs.update(dict(pooling=pooling, l2_norm=l2_norm, prefix=prefix))
-    if _encoder_class == 'uniir':
-        kwargs.update(dict(l2_norm=True))
     if _encoder_class == 'clip' or 'clip' in encoder:
         kwargs.update(dict(l2_norm=True, prefix=prefix, multimodal=multimodal))
     if _encoder_class == 'uniir':
