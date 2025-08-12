af055f3927abd284fc6d1ef886a26e86c38809a7
Daniel Guo <82596509+clides@users.noreply.github.com>
2025-08-11 21:57:50 -0400

    Integration of UniIR into pyserini's search (#2216)
    - added missing *.yaml to .toml for uniir model configs
    - integrated mbeir retrieval with uniir models. The query instructions must be downloaded from the https://huggingface.co/datasets/TIGER-Lab/M-BEIR/blob/main/instructions/query_instructions.tsv

--- pyserini/encode/_uniir.py
+++ pyserini/encode/_uniir.py
@@ -15,22 +15,24 @@
 #
 
 import yaml
+import random
 import importlib.resources
 from abc import ABC, abstractmethod
-from typing import Any, List
+from typing import Any, List, Optional
 from types import SimpleNamespace
 from importlib.resources import files
 
 import torch
 import faiss
-from huggingface_hub import hf_hub_download
+import pandas as pd
 from torch.utils.data import DataLoader
-
-from pyserini.encode.mbeir.mbeir_dataset import MBEIRCorpusDataset
+from huggingface_hub import hf_hub_download
+from pyserini.encode.mbeir.mbeir_dataset import MBEIRCorpusDataset, MBEIRQueryDataset
 from pyserini.encode.mbeir.uniir import (BLIPFeatureFusion, BLIPScoreFusion,
                             CLIPFeatureFusion, CLIPScoreFusion,
-                            MBEIRCandidatePoolCollator, generate_embeds_and_ids_for_dataset_with_gather,
-                            format_string, hash_did)
+                            MBEIRCandidatePoolCollator, MBEIRInferenceOnlyCollator,
+                            generate_embeds_and_ids_for_dataset_with_gather,
+                            format_string, hash_did, hash_qid)
 
 
 MODEL_REGISTRY = {
@@ -136,3 +138,104 @@ class UniIRCorpusEncoder(UniIREncoder):
             corpus_embeddings = corpus_embeddings.astype('float16') if use_fp16 else corpus_embeddings
 
         return corpus_embeddings
+
+
+class UniIRQueryEncoder(UniIREncoder):
+    def __init__(
+        self,
+        encoder_dir: str,
+        device="cuda:0",
+        l2_norm=False,
+        instruction_config=None,
+        **kwargs: Any,
+    ):
+        if instruction_config:
+            instructions, candidate_modality, randomize_instructions = (
+                self._load_instruction_config(instruction_config)
+            )
+            self._instructions = instructions
+            self._cand_modality = candidate_modality
+            self._randomize_instructions = randomize_instructions
+        super().__init__(encoder_dir, device, l2_norm, **kwargs)
+
+    def _load_instruction_config(self, instruction_config):
+        try:
+            with open(instruction_config, "r") as f:
+                config = yaml.safe_load(f)
+            instruction_file = config.get("instruction_file", None)
+            candidate_modality = config.get("candidate_modality", None)
+            dataset_id = config.get("dataset_id", None)
+            randomize_instructions = config.get("randomize_instructions", False)
+            if not instruction_file or not candidate_modality or not dataset_id:
+                raise ValueError(
+                    "Instruction file, candidate_modality, or dataset_id is missing in the config. Please download the instruction file from https://huggingface.co/datasets/TIGER-Lab/M-BEIR/blob/main/instructions/query_instructions.tsv"
+                )
+        except Exception as e:
+            raise ValueError(f"Error loading instruction config: {e}")
+
+        try:
+            df = pd.read_csv(instruction_file, sep="\t")
+            filtered = df[df["dataset_id"].astype(int) == int(dataset_id)]
+            instructions = filtered.to_dict(orient="records")
+
+            return instructions, candidate_modality, randomize_instructions
+        except Exception as e:
+            raise ValueError(f"Error reading instruction or corpus file: {e}. Please download the instruction file from https://huggingface.co/datasets/TIGER-Lab/M-BEIR/blob/main/instructions/query_instructions.tsv")
+
+    def _get_instruction_prompt(self, q_modality) -> Optional[str]:
+        instructions = self._instructions
+        for instruction in instructions:
+            if (
+                instruction["query_modality"] == q_modality
+                and instruction["cand_modality"] == self._cand_modality
+            ):
+                if self._randomize_instructions:
+                    prompts = [
+                        instruction[k] for k in instruction if k.startswith("prompt_")
+                    ]
+                    return random.choice(prompts) if prompts else None
+                else:
+                    return instruction["prompt_1"]
+
+    def encode(
+        self,
+        qid: int,
+        query_txt: str,
+        query_img_path: str,
+        query_modality: str,
+        pos_cand_list: List[str],
+        **kwargs: Any,
+    ):
+        use_fp16 = kwargs.get("fp16", False)
+
+        if hasattr(self, "_instructions"):
+            prompt = self._get_instruction_prompt(q_modality=query_modality)
+            if prompt is not None:
+                query_txt = f"{prompt} {query_txt}" if query_txt else prompt
+
+        query_info = [{
+            "qid": hash_qid(qid),
+            "query_txt": format_string(query_txt),
+            "query_img_path": query_img_path,
+            "query_modality": query_modality,
+        }]
+
+        dataset = MBEIRQueryDataset(query_info, self.img_preprocess_fn)
+        collator = MBEIRInferenceOnlyCollator(
+            tokenizer=self.tokenizer, image_size=(224, 224)
+        )
+        dataloader = DataLoader(dataset, batch_size=1, collate_fn=collator)
+
+        query_embeddings, _ = generate_embeds_and_ids_for_dataset_with_gather(  
+            self.model,  
+            dataloader,  
+            device=self.device,  
+            use_fp16=use_fp16,  
+        )  
+
+        if self.l2_norm:
+            query_embeddings = query_embeddings.astype('float32')
+            faiss.normalize_L2(query_embeddings)
+            query_embeddings = query_embeddings.astype('float16') if use_fp16 else query_embeddings
+
+        return query_embeddings
