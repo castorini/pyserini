a54a14d6359e583b98098a2bb95feee022ec06d4
clides <daniel168.guo@gmail.com>
2025-09-20 14:35:53 -0400

    fixes

--- tests/test_encoder_uniir.py
+++ tests/test_encoder_uniir.py
@@ -1,16 +1,35 @@
 import sys
+import numpy as np
 import unittest
 from unittest.mock import patch, MagicMock
 
-class TestUniIREncoder(unittest.TestCase):
-    def setUp(self):
-        # Clear cached imports before each test
-        modules_to_clear = [k for k in sys.modules.keys() if k.startswith('pyserini')]
-        for module in modules_to_clear:
-            if module in sys.modules:
-                del sys.modules[module]
+MOCK_CORPUS_EMBEDDINGS = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]])
+MOCK_QUERY_EMBEDDINGS = np.array([[0.1, 0.2, 0.3]])
 
-    def test_uniir_encoder_missing_package(self):
+class TestUniIREncoder(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_corpus_encoder = MagicMock()
+        cls.mock_corpus_encoder.encode.return_value = MOCK_CORPUS_EMBEDDINGS
+        
+        cls.mock_query_encoder = MagicMock()
+        cls.mock_query_encoder.encode.return_value = MOCK_QUERY_EMBEDDINGS
+
+        cls.patcher = patch.dict('sys.modules', {
+            'uniir_for_pyserini': MagicMock(),
+            'uniir_for_pyserini.pyserini_integration': MagicMock(),
+            'uniir_for_pyserini.pyserini_integration.uniir_corpus_encoder':
+                MagicMock(CorpusEncoder=lambda **kwargs: cls.mock_corpus_encoder),
+            'uniir_for_pyserini.pyserini_integration.uniir_query_encoder':
+                MagicMock(QueryEncoder=lambda **kwargs: cls.mock_query_encoder),
+        })
+        cls.patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.patcher.stop()
+
+    def test_uniir_corpus_encoder_missing_package(self):
         """Test UniIR corpus encoder fails when uniir-for-pyserini package is missing"""
         with patch.dict('sys.modules', {'uniir_for_pyserini': None}):
             from pyserini.encode.__main__ import init_encoder
@@ -37,71 +56,55 @@ class TestUniIREncoder(unittest.TestCase):
                 )
             self.assertIn("uniir-for-pyserini package is not installed", str(context.exception))
 
-    def test_uniir_encoder_with_package_available(self):
+    def test_uniir_corpus_encoder_with_package_available(self):
         """Test UniIR encoder works when package is available (mocked)"""
-        import numpy as np
-
-        MOCK_CORPUS_EMBEDDINGS = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]])
-        MOCK_QUERY_EMBEDDINGS = np.array([[0.1, 0.2, 0.3]])
+        from pyserini.encode.__main__ import init_encoder
         
-        mock_corpus_encoder = MagicMock()
-        mock_corpus_encoder.encode.return_value = MOCK_CORPUS_EMBEDDINGS
+        corpus_encoder = init_encoder('clip_sf_large', 'uniir', 'cpu', 'cls', True, None, True)
+        self.assertIsNotNone(corpus_encoder)
         
-        mock_query_encoder = MagicMock()
-        mock_query_encoder.encode.return_value = MOCK_QUERY_EMBEDDINGS
-        
-        with patch.dict('sys.modules', {
-            'uniir_for_pyserini': MagicMock(),
-            'uniir_for_pyserini.pyserini_integration': MagicMock(),
-            'uniir_for_pyserini.pyserini_integration.uniir_corpus_encoder': MagicMock(CorpusEncoder=lambda **kwargs: mock_corpus_encoder),
-            'uniir_for_pyserini.pyserini_integration.uniir_query_encoder': MagicMock(QueryEncoder=lambda **kwargs: mock_query_encoder)
-        }):
-            from pyserini.encode.__main__ import init_encoder
-            
-            corpus_encoder = init_encoder('clip_sf_large', 'uniir', 'cpu', 'cls', True, None, True)
-            self.assertIsNotNone(corpus_encoder)
-            
-            test_dids = ["1", "2", "3"]
-            test_img_paths = ["test1.jpg", None, "test3.jpg"]
-            test_txts = [None, "test", "test"]
-            test_modalitys = ["image", "text", "image,text"]
-              
-            corpus_embeddings = corpus_encoder.encode(
-                dids=test_dids,
-                img_paths=test_img_paths,
-                modalitys=test_modalitys,
-                txts=test_txts
-            )
-             
-            self.assertEqual(len(corpus_embeddings), 3)  
-            self.assertEqual(corpus_embeddings.shape[1], 3)
-            np.testing.assert_array_equal(corpus_embeddings, MOCK_CORPUS_EMBEDDINGS)
+        test_dids = ["1", "2", "3"]
+        test_img_paths = ["test1.jpg", None, "test3.jpg"]
+        test_txts = [None, "test", "test"]
+        test_modalitys = ["image", "text", "image,text"]
+          
+        corpus_embeddings = corpus_encoder.encode(
+            dids=test_dids,
+            img_paths=test_img_paths,
+            modalitys=test_modalitys,
+            txts=test_txts
+        )
+         
+        self.assertEqual(len(corpus_embeddings), 3)  
+        self.assertEqual(corpus_embeddings.shape[1], 3)
+        np.testing.assert_array_equal(corpus_embeddings, MOCK_CORPUS_EMBEDDINGS)
 
-            from pyserini.search.faiss.__main__ import init_query_encoder
-            query_encoder = init_query_encoder(
-                encoder='clip_sf_large',
-                encoder_class='uniir',
-                tokenizer_name=None,
-                topics_name=None,
-                encoded_queries=None,
-                device='cpu',
-                max_length=512,
-                pooling='cls',
-                l2_norm=True,
-                prefix=None,
-            )
-            self.assertIsNotNone(query_encoder)
+    def test_uniir_query_encoder_with_package_available(self):
+        from pyserini.search.faiss.__main__ import init_query_encoder
+        query_encoder = init_query_encoder(
+            encoder='clip_sf_large',
+            encoder_class='uniir',
+            tokenizer_name=None,
+            topics_name=None,
+            encoded_queries=None,
+            device='cpu',
+            max_length=512,
+            pooling='cls',
+            l2_norm=True,
+            prefix=None,
+        )
+        self.assertIsNotNone(query_encoder)
 
-            query_embeddings = query_encoder.encode(
-                qid=1,
-                query_txt="test query",
-                query_img_path="test_query.jpg",
-                query_modality="image,text"
-            )
+        query_embeddings = query_encoder.encode(
+            qid=1,
+            query_txt="test query",
+            query_img_path="test_query.jpg",
+            query_modality="image,text"
+        )
 
-            self.assertEqual(len(query_embeddings), 1)
-            self.assertEqual(query_embeddings.shape[1], 3)
-            np.testing.assert_array_equal(query_embeddings, MOCK_QUERY_EMBEDDINGS)
+        self.assertEqual(len(query_embeddings), 1)
+        self.assertEqual(query_embeddings.shape[1], 3)
+        np.testing.assert_array_equal(query_embeddings, MOCK_QUERY_EMBEDDINGS)
   
 if __name__ == '__main__':
     unittest.main()
