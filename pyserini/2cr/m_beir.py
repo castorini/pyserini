5b5a4e46d52e44e88295086b18413700e4b0f3ac
clides <daniel168.guo@gmail.com>
2026-01-15 20:37:09 -0500

    small fix

--- pyserini/2cr/m_beir.py
+++ pyserini/2cr/m_beir.py
@@ -41,6 +41,7 @@ trec_eval_metric_definitions = {
 def format_run_command(raw):
     return (
         raw.replace("--topics", "\\\n  --topics")
+        .replace('--encoder', '\\\n  --encoder')
         .replace("--index", "\\\n  --index")
         .replace("--output-format", "\\\n  --output-format")
         .replace("--output ", "\\\n  --output ")
