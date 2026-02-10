b252eb8c09f3400c2fd0e2f100442558d5b47d9e
Daniel Guo <82596509+clides@users.noreply.github.com>
2026-02-09 20:18:27 -0500

    Update mcp search tool to support images as urls (#2415)

--- pyserini/server/mcp/tools.py
+++ pyserini/server/mcp/tools.py
@@ -40,12 +40,12 @@ def register_tools(mcp: FastMCP, controller: SearchController):
         ef_search: int = 100,
         encoder: str = None,
         query_generator: str = None
-    ) -> list:
+    ):
         """
         Search the Pyserini index with the appropriate method for the type of the index provided and return top-k hits.
 
         Args:
-            query: Search query dictionary with format {"qid": "...", "query_txt": "...", "query_img_path": "..."} where query_txt and query_img_path are optional but at least one must be provided
+            query: Search query dictionary with format {"qid": "...", "query_txt": "...", "query_img_path": "..."} where query_txt and query_img_path are optional but at least one must be provided (query_img_path can either be a local path of an image or an url to an image)
             index_name: Name of index to search, use the list_indexes tool to see available indexes
             instruction_config: for instruction guided search for multimodal embedding models
             k: Number of results to return (default: 10)
@@ -91,7 +91,7 @@ def register_tools(mcp: FastMCP, controller: SearchController):
         return final_output
 
     @mcp.tool()
-    def get_document(docid: str, index_name: str) -> list:
+    def get_document(docid: str, index_name: str):
         """
         Retrieve the full text and image (if available) of a document by its ID from a given index.
 
