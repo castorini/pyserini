46352e8aa352e7476be3fbb9a82d5977247285b3
Jimmy Lin <jimmylin@uwaterloo.ca>
2026-06-27 08:27:47 -0400

    Refactor archive download utility (#2584)

--- pyserini/util.py
+++ pyserini/util.py
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-from collections.abc import Iterator
-from contextlib import contextmanager
 import hashlib
 import importlib
 import logging
@@ -24,6 +22,8 @@ import re
 import shutil
 import tarfile
 import urllib.request
+from collections.abc import Iterator
+from contextlib import contextmanager
 from urllib.error import HTTPError, URLError
 
 import pandas as pd
@@ -32,8 +32,13 @@ from tqdm import tqdm
 from pyserini.encoded_corpus_info import CORPUS_INFO
 from pyserini.encoded_query_info import QUERY_INFO
 from pyserini.evaluate_script_info import EVALUATION_INFO
-from pyserini.prebuilt_index_info import TF_INDEX_INFO, IMPACT_INDEX_INFO, \
-    LUCENE_HNSW_INDEX_INFO, LUCENE_FLAT_INDEX_INFO, FAISS_INDEX_INFO
+from pyserini.prebuilt_index_info import (
+    FAISS_INDEX_INFO,
+    IMPACT_INDEX_INFO,
+    LUCENE_FLAT_INDEX_INFO,
+    LUCENE_HNSW_INDEX_INFO,
+    TF_INDEX_INFO,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -58,7 +63,7 @@ def temporary_env(**env_vars: str | None) -> Iterator[None]:
                 os.environ[key] = previous_value
 
 
-def _archive_name_to_index_name(archive_name):
+def _archive_name_to_dir_name(archive_name):
     return re.sub(r'''\.tar(\.gz)?(\?.*)?$''', '', archive_name)
 
 
@@ -237,7 +242,7 @@ def download_url(url, save_dir, local_filename=None, md5=None, force=False, verb
             print(f'{destination_path} already exists!')
         if not force:
             if verbose:
-                print(f'Skipping download.')
+                print('Skipping download.')
             return destination_path
         if verbose:
             print(f'force=True, removing {destination_path}; fetching fresh copy...')
@@ -247,7 +252,7 @@ def download_url(url, save_dir, local_filename=None, md5=None, force=False, verb
 
     # Note: urlretrieve doesn't directly support headers.
     try:
-        with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=filename) as t:
+        with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=filename, disable=not verbose) as t:
             with urllib.request.urlopen(req) as response:
                 # Get file size from headers if available
                 file_size = int(response.headers.get('Content-Length', -1))
@@ -266,9 +271,7 @@ def download_url(url, save_dir, local_filename=None, md5=None, force=False, verb
     # Don't do the size check if the size is -1 or None.
     if expected_size != -1 and expected_size is not None:
         actual_size = os.path.getsize(destination_path)
-        assert actual_size == expected_size, (
-            f'{destination_path} does not match expected file size! Expecting {expected_size} bytes, got {actual_size} bytes.'
-        )
+        assert actual_size == expected_size, f'{destination_path} does not match expected file size! Expecting {expected_size} bytes, got {actual_size} bytes.'
 
     if md5:
         md5_computed = compute_md5(destination_path)
@@ -283,60 +286,60 @@ def get_cache_home():
         return custom_dir
     return os.path.expanduser(os.path.join(f'~{os.path.sep}.cache', "pyserini"))
 
-def download_and_unpack_index(url, index_directory='indexes', local_filename=False, md5=None,
-                              force=False, verbose=True, prebuilt=False, expected_size=None):
+def download_and_unpack_archive(url, output_dir='indexes', local_filename=False, md5=None,
+                                force=False, verbose=True, append_md5_to_dir_name=False, expected_size=None):
     archive_name = _download_archive_name(url, local_filename)
-    index_name = _archive_name_to_index_name(archive_name)
+    dir_name = _archive_name_to_dir_name(archive_name)
 
-    if prebuilt:
-        index_directory = os.path.join(get_cache_home(), index_directory)
-        index_path = os.path.join(index_directory, f'{index_name}.{md5}')
+    if append_md5_to_dir_name:
+        output_dir = os.path.join(get_cache_home(), output_dir)
+        output_path = os.path.join(output_dir, f'{dir_name}.{md5}')
 
-        if not os.path.exists(index_directory):
-            os.makedirs(index_directory)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-        local_tarball = os.path.join(index_directory, archive_name)
+        local_tarball = os.path.join(output_dir, archive_name)
         # If there's a local tarball, it's likely corrupted, because we remove the local tarball on success (below).
         # So, we want to remove.
         if os.path.exists(local_tarball):
             os.remove(local_tarball)
     else:
-        local_tarball = os.path.join(index_directory, archive_name)
-        index_path = os.path.join(index_directory, f'{index_name}')
+        local_tarball = os.path.join(output_dir, archive_name)
+        output_path = os.path.join(output_dir, f'{dir_name}')
 
-    # Check to see if index already exists, if so, simply return (quietly) unless force=True, in which case we remove
-    # index and download fresh copy.
-    if os.path.exists(index_path):
+    # Check to see if the extracted directory already exists. If so, return it unless force=True, in which case we
+    # remove it and download a fresh copy.
+    if os.path.exists(output_path):
         if not force:
             if verbose:
-                print(f'{index_path} already exists, skipping download.')
-            return index_path
+                print(f'{output_path} already exists, skipping download.')
+            return output_path
         if verbose:
-            print(f'{index_path} already exists, but force=True, removing {index_path} and fetching fresh copy...')
-        shutil.rmtree(index_path)
+            print(f'{output_path} already exists, but force=True, removing {output_path} and fetching fresh copy...')
+        shutil.rmtree(output_path)
 
     if verbose:
-        print(f'Downloading index at {url}...')
-
-    local_tarball = download_url(url, index_directory, local_filename=local_filename, verbose=False, md5=md5, expected_size=expected_size)
+        print(f'Downloading archive at {url}...')
+    local_tarball = download_url(url, output_dir, local_filename=local_filename, verbose=False, md5=md5, expected_size=expected_size)
 
     if verbose:
-        print(f'Extracting {local_tarball} into {index_path}...')
+        print(f'Extracting {local_tarball} into {output_path}...')
+
     tarball = tarfile.open(local_tarball)
     dirs_in_tarball = [member.name for member in tarball if member.isdir()]
     assert len(dirs_in_tarball), f"Detect multiple members ({', '.join(dirs_in_tarball)}) under the tarball {local_tarball}."
-    tarball.extractall(index_directory, filter='data')
+    tarball.extractall(output_dir, filter='data')
     tarball.close()
     os.remove(local_tarball)
 
-    if prebuilt:
+    if append_md5_to_dir_name:
         dir_in_tarball = dirs_in_tarball[0]
-        if dir_in_tarball != index_name:
-            logger.info(f"Renaming {index_directory}/{dir_in_tarball} into {index_directory}/{index_name}.")
-            index_name = dir_in_tarball
-        os.rename(os.path.join(index_directory, f'{index_name}'), index_path)
+        if dir_in_tarball != dir_name:
+            logger.info(f"Renaming {os.path.join(output_dir, dir_in_tarball)} into {output_path}.")
+            dir_name = dir_in_tarball
+        os.rename(os.path.join(output_dir, f'{dir_name}'), output_path)
 
-    return index_path
+    return output_path
 
 
 def check_downloaded(index_name):
@@ -353,7 +356,7 @@ def check_downloaded(index_name):
     index_url = target_index['urls'][0]
     index_md5 = target_index['md5']
     archive_name = target_index['filename'] if 'filename' in target_index else _download_archive_name(index_url, None)
-    index_name = _archive_name_to_index_name(archive_name)
+    index_name = _archive_name_to_dir_name(archive_name)
     index_directory = os.path.join(get_cache_home(), 'indexes')
     index_path = os.path.join(index_directory, f'{index_name}.{index_md5}')
 
@@ -414,11 +417,10 @@ def download_prebuilt_index(index_name, force=False, verbose=True, mirror=None):
     for url in target_index['urls']:
         local_filename = target_index['filename'] if 'filename' in target_index else None
         try:
-            return download_and_unpack_index(url, local_filename=local_filename, prebuilt=True,
-                                             md5=index_md5, verbose=verbose, expected_size=expected_size)
-        except (HTTPError, URLError) as e:
+            return download_and_unpack_archive(url, local_filename=local_filename, append_md5_to_dir_name=True, md5=index_md5, verbose=verbose, expected_size=expected_size)
+        except (HTTPError, URLError):
             print(f'Unable to download prebuilt index at {url}, trying next URL...')
-    raise ValueError(f'Unable to download prebuilt index at any known URLs.')
+    raise ValueError('Unable to download prebuilt index at any known URLs.')
 
 
 def download_encoded_queries(query_name, force=False, verbose=True, mirror=None):
@@ -427,10 +429,10 @@ def download_encoded_queries(query_name, force=False, verbose=True, mirror=None)
     query_md5 = QUERY_INFO[query_name]['md5']
     for url in QUERY_INFO[query_name]['urls']:
         try:
-            return download_and_unpack_index(url, index_directory='queries', prebuilt=True, verbose=verbose, md5=query_md5)
-        except (HTTPError, URLError) as e:
+            return download_and_unpack_archive(url, output_dir='queries', append_md5_to_dir_name=True, verbose=verbose, md5=query_md5)
+        except (HTTPError, URLError):
             print(f'Unable to download encoded query at {url}, trying next URL...')
-    raise ValueError(f'Unable to download encoded query at any known URLs.')
+    raise ValueError('Unable to download encoded query at any known URLs.')
 
 
 def download_encoded_corpus(corpus_name, force=False, verbose=True, mirror=None):
@@ -440,10 +442,10 @@ def download_encoded_corpus(corpus_name, force=False, verbose=True, mirror=None)
     for url in CORPUS_INFO[corpus_name]['urls']:
         local_filename = CORPUS_INFO[corpus_name]['filename'] if 'filename' in CORPUS_INFO[corpus_name] else None
         try:
-            return download_and_unpack_index(url, local_filename=local_filename, index_directory='corpus', prebuilt=True, md5=corpus_md5)
-        except (HTTPError, URLError) as e:
+            return download_and_unpack_archive(url, local_filename=local_filename, output_dir='corpus', append_md5_to_dir_name=True, md5=corpus_md5)
+        except (HTTPError, URLError):
             print(f'Unable to download encoded corpus at {url}, trying next URL...')
-    raise ValueError(f'Unable to download encoded corpus at any known URLs.')
+    raise ValueError('Unable to download encoded corpus at any known URLs.')
 
 
 def download_evaluation_script(evaluation_name, force=False, verbose=True, mirror=None):
@@ -457,7 +459,7 @@ def download_evaluation_script(evaluation_name, force=False, verbose=True, mirro
             return download_url(url, save_dir=save_dir)
         except HTTPError:
             print(f'Unable to download evaluation script at {url}, trying next URL...')
-    raise ValueError(f'Unable to download evaluation script at any known URLs.')
+    raise ValueError('Unable to download evaluation script at any known URLs.')
 
 
 def get_sparse_index(index_name):
