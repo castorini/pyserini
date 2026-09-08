308c2a64c583fd26ec03645036b9d4ba41ae47d0
appleweiping <vipinapple986@gmail.com>
2026-09-07 22:49:25 -0500

    Fix BRIGHT end timestamp

--- pyserini/2cr/bright.py
+++ pyserini/2cr/bright.py
@@ -264,7 +264,7 @@ def run_conditions(args):
 
     end = time.time()
     start_str = datetime.fromtimestamp(start, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-    end_str = datetime.fromtimestamp(start, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+    end_str = datetime.fromtimestamp(end, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
 
     print(f'Start time: {start_str}')
     print(f'End time: {end_str}')
